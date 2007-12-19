93c0c94585fccea1a761492bb9df18195d1fcd47
kohsuke <kohsuke@dev.java.net>
2007-12-19 01:59:25 +0000

    importing Git plugin from http://www.nabble.com/git-plugin-p14399004.html

---
+++ src/main/java/hudson/plugins/git/GitChangeLogParser.java
@@ -0,0 +1,55 @@
+package hudson.plugins.git;
+
+import hudson.model.AbstractBuild;
+import hudson.scm.ChangeLogParser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xml.sax.SAXException;
+
+/**
+ * Parse the git log
+ * @author Nigel Magnay
+ */
+public class GitChangeLogParser extends ChangeLogParser {
+	public GitChangeSetList parse(AbstractBuild build, File changelogFile)
+			throws IOException, SAXException {
+
+		ArrayList<GitChangeSet> r = new ArrayList<GitChangeSet>();
+
+		// Parse the log file into GitChangeSet items - each one is a commit
+
+		BufferedReader rdr = new BufferedReader(new FileReader(changelogFile));
+
+		String line;
+		List<String> lines = null;
+
+		while ((line = rdr.readLine()) != null) {
+			if (line.startsWith("commit ")) {
+				if (lines != null) {
+					r.add(parseCommit(lines));
+				}
+				lines = new ArrayList<String>();
+			}
+			
+
+			lines.add(line);
+		}
+
+		if (lines != null) {
+			r.add(parseCommit(lines));
+		}
+
+		return new GitChangeSetList(build, r);
+	}
+
+	private GitChangeSet parseCommit(List<String> lines) {
+		return new GitChangeSet(lines);
+	}
+
+}
---
+++ src/main/java/hudson/plugins/git/GitChangeSetList.java
@@ -0,0 +1,36 @@
+package hudson.plugins.git;
+
+import hudson.scm.ChangeLogSet;
+import hudson.model.AbstractBuild;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.Iterator;
+
+/**
+ * List of changeset that went into a particular build.
+ * @author Nigel Magnay
+ */
+public class GitChangeSetList extends ChangeLogSet<GitChangeSet> {
+    private final List<GitChangeSet> changeSets;
+
+    /*package*/ GitChangeSetList(AbstractBuild build, List<GitChangeSet> logs) {
+        super(build);
+        Collections.reverse(logs);  // put new things first
+        this.changeSets = Collections.unmodifiableList(logs);
+        for (GitChangeSet log : logs)
+            log.setParent(this);
+    }
+
+    public boolean isEmptySet() {
+        return changeSets.isEmpty();
+    }
+
+    public Iterator<GitChangeSet> iterator() {
+        return changeSets.iterator();
+    }
+
+    public List<GitChangeSet> getLogs() {
+        return changeSets;
+    }
+}
