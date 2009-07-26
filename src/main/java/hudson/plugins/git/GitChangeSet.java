c448c38b8ac1b35aca6c0900fde4054bb2158fb2
Josh Beitelspacher <josh@netbeetle.com>
2009-07-26 12:53:23 -0500

    Set user email from Git email
    If the user does not already exist create a new user when the changeset is
    parsed and set the user name from the email address.  If the user already
    exists return it without modification.

--- src/main/java/hudson/plugins/git/GitChangeSet.java
+++ src/main/java/hudson/plugins/git/GitChangeSet.java
@@ -2,7 +2,9 @@ package hudson.plugins.git;
 
 import hudson.model.User;
 import hudson.scm.ChangeLogSet;
+import hudson.tasks.Mailer;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -13,13 +15,13 @@ import java.util.List;
  */
 public class GitChangeSet extends ChangeLogSet.Entry {
 
-	String author;
+	private String author;
+	private String authorEmail;
 	private String comment;
 	private String title;
 	private String id;
 
-	
-	Collection<String> affectedPaths = new HashSet<String>();
+	private Collection<String> affectedPaths = new HashSet<String>();
 
 	public GitChangeSet(List<String> lines) {
 		if (lines.size() > 0) {
@@ -40,6 +42,7 @@ public class GitChangeSet extends ChangeLogSet.Entry {
 					// parent
 				} else if (line.startsWith("committer ")) {
 					this.author = line.substring(10, line.indexOf(" <"));
+					this.authorEmail = line.substring(line.indexOf(" <") + 2, line.indexOf("> "));
 				} else if (line.startsWith("author ")) {
 				} else if (line.startsWith("    ")) {
 					comment += line.substring(4) + "\n";
@@ -75,7 +78,20 @@ public class GitChangeSet extends ChangeLogSet.Entry {
 	public User getAuthor() {
 		if (this.author == null)
 			throw new RuntimeException("No author in this changeset!");
-		return User.get(this.author, true);
+
+		User user = User.get(this.author, false);
+
+		// if the user doesn't exist create it and set the email address
+		if (user == null) {
+			user = User.get(this.author, true);
+			try {
+				user.addProperty(new Mailer.UserProperty(this.authorEmail));
+			} catch (IOException e) {
+				// ignore error
+			}
+		}
+
+		return user;
 	}
 
 	@Override
