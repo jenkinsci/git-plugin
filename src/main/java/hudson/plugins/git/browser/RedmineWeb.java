46f668dd54b9707d5bf715fe1ad8c286c4b789f4
Mirko Friedenhagen <mfriedenhagen@dev.java.net>
2010-11-30 22:19:24 +0100

    Implement support for the redmine browser.

---
+++ src/main/java/hudson/plugins/git/browser/RedmineWeb.java
@@ -0,0 +1,104 @@
+package hudson.plugins.git.browser;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.plugins.git.GitChangeSet;
+import hudson.plugins.git.GitChangeSet.Path;
+import hudson.plugins.git.GitRepositoryBrowser;
+import hudson.scm.EditType;
+import hudson.scm.RepositoryBrowser;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Git Browser for <a href="http://www.redmine.org/">Redmine</a>.
+ * 
+ * @author mfriedenhagen
+ */
+public class RedmineWeb extends GitRepositoryBrowser {
+
+    private static final long serialVersionUID = 1L;
+
+    private final URL url;
+
+    @DataBoundConstructor
+    public RedmineWeb(String url) throws MalformedURLException {
+        this.url = normalizeToEndWithSlash(new URL(url));
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    @Override
+    public URL getChangeSetLink(GitChangeSet changeSet) throws IOException {
+        return new URL(url, "revisions/" + changeSet.getId().toString());
+    }
+
+    /**
+     * Creates a link to the file diff.
+     * 
+     * https://SERVER/PATH/projects/PROJECT/repository/revisions/a9182a07750c9a0dfd89a8461adf72ef5ef0885b/diff/pom.xml
+     * 
+     * Returns a diff link for {@link EditType#DELETE} and {@link EditType#EDIT}, for {@link EditType#ADD} returns an
+     * {@link RedmineWeb#getFileLink(Path)}.
+     * 
+     * 
+     * @param path
+     *            affected file path
+     * @return diff link
+     * @throws IOException
+     */
+    @Override
+    public URL getDiffLink(Path path) throws IOException {
+        final GitChangeSet changeSet = path.getChangeSet();
+        final URL changeSetLink = getChangeSetLink(changeSet);
+        final URL difflink;
+        if (path.getEditType().equals(EditType.ADD)) {
+            difflink = getFileLink(path);
+        } else {
+            difflink = new URL(changeSetLink, changeSetLink.getPath() + "/diff/" + path.getPath());
+        }
+        return difflink;
+    }
+
+    /**
+     * Creates a link to the file.
+     * https://SERVER/PATH/projects/PROJECT/repository/revisions/a9182a07750c9a0dfd89a8461adf72ef5ef0885b/entry/pom.xml
+     * For deleted files just returns a diff link, which will have /dev/null as target file.
+     * 
+     * @param path
+     *            file
+     * @return file link
+     * @throws IOException
+     */
+    @Override
+    public URL getFileLink(Path path) throws IOException {
+        if (path.getEditType().equals(EditType.DELETE)) {
+            return getDiffLink(path);
+        } else {
+            final String spec = "revisions/" + path.getChangeSet().getId() + "/entry/" + path.getPath();
+            return new URL(url, url.getPath() + spec);
+        }
+    }
+
+    @Extension
+    public static class RedmineWebDescriptor extends Descriptor<RepositoryBrowser<?>> {
+        public String getDisplayName() {
+            return "redmineweb";
+        }
+
+        @Override
+        public RedmineWeb newInstance(StaplerRequest req, JSONObject jsonObject) throws FormException {
+            return req.bindParameters(RedmineWeb.class, "redmineweb.");
+        }
+    }
+
+}
---
+++ src/test/java/hudson/plugins/git/browser/RedmineWebTest.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2010 Mirko Friedenhagen
+ */
+
+package hudson.plugins.git.browser;
+
+import hudson.plugins.git.GitChangeLogParser;
+import hudson.plugins.git.GitChangeSet;
+import hudson.plugins.git.GitChangeSet.Path;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.xml.sax.SAXException;
+
+/**
+ * @author mirko
+ *
+ */
+public class RedmineWebTest extends TestCase {
+
+    /**
+     *
+     */
+    private static final String REDMINE_URL = "https://SERVER/PATH/projects/PROJECT/repository";
+    private final RedmineWeb redmineWeb;
+
+    {
+        try {
+            redmineWeb = new RedmineWeb(REDMINE_URL);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Test method for {@link hudson.plugins.git.browser.RedmineWeb#getUrl()}.
+     * @throws MalformedURLException
+     */
+    public void testGetUrl() throws MalformedURLException {
+        assertEquals(String.valueOf(redmineWeb.getUrl()), REDMINE_URL  + "/");
+    }
+
+    /**
+     * Test method for {@link hudson.plugins.git.browser.RedmineWeb#getUrl()}.
+     * @throws MalformedURLException
+     */
+    public void testGetUrlForRepoWithTrailingSlash() throws MalformedURLException {
+        assertEquals(String.valueOf(new RedmineWeb(REDMINE_URL + "/").getUrl()), REDMINE_URL  + "/");
+    }
+
+    /**
+     * Test method for {@link hudson.plugins.git.browser.RedmineWeb#getChangeSetLink(hudson.plugins.git.GitChangeSet)}.
+     * @throws SAXException
+     * @throws IOException
+     */
+    public void testGetChangeSetLinkGitChangeSet() throws IOException, SAXException {
+        final URL changeSetLink = redmineWeb.getChangeSetLink(createChangeSet("rawchangelog"));
+        assertEquals(REDMINE_URL + "/revisions/396fc230a3db05c427737aa5c2eb7856ba72b05d", changeSetLink.toString());
+    }
+
+    /**
+     * Test method for {@link hudson.plugins.git.browser.RedmineWeb#getDiffLink(hudson.plugins.git.GitChangeSet.Path)}.
+     * @throws SAXException
+     * @throws IOException
+     */
+    public void testGetDiffLinkPath() throws IOException, SAXException {
+        final HashMap<String, Path> pathMap = createPathMap("rawchangelog");
+        final Path modified1 = pathMap.get("src/main/java/hudson/plugins/git/browser/GithubWeb.java");
+        assertEquals(REDMINE_URL + "/revisions/396fc230a3db05c427737aa5c2eb7856ba72b05d/diff/src/main/java/hudson/plugins/git/browser/GithubWeb.java", redmineWeb.getDiffLink(modified1).toString());
+        final Path modified2 = pathMap.get("src/test/java/hudson/plugins/git/browser/GithubWebTest.java");
+        assertEquals(REDMINE_URL + "/revisions/396fc230a3db05c427737aa5c2eb7856ba72b05d/diff/src/test/java/hudson/plugins/git/browser/GithubWebTest.java", redmineWeb.getDiffLink(modified2).toString());
+        // For added files returns a link to the entry.
+        final Path added = pathMap.get("src/test/resources/hudson/plugins/git/browser/rawchangelog-with-deleted-file");
+        assertEquals(REDMINE_URL + "/revisions/396fc230a3db05c427737aa5c2eb7856ba72b05d/entry/src/test/resources/hudson/plugins/git/browser/rawchangelog-with-deleted-file", redmineWeb.getDiffLink(added).toString());
+    }
+
+    /**
+     * Test method for {@link hudson.plugins.git.browser.GithubWeb#getFileLink(hudson.plugins.git.GitChangeSet.Path)}.
+     * @throws SAXException
+     * @throws IOException
+     */
+    public void testGetFileLinkPath() throws IOException, SAXException {
+        final HashMap<String,Path> pathMap = createPathMap("rawchangelog");
+        final Path path = pathMap.get("src/main/java/hudson/plugins/git/browser/GithubWeb.java");
+        final URL fileLink = redmineWeb.getFileLink(path);
+        assertEquals(REDMINE_URL  + "/revisions/396fc230a3db05c427737aa5c2eb7856ba72b05d/entry/src/main/java/hudson/plugins/git/browser/GithubWeb.java", String.valueOf(fileLink));
+    }
+
+    /**
+     * Test method for {@link hudson.plugins.git.browser.GithubWeb#getFileLink(hudson.plugins.git.GitChangeSet.Path)}.
+     * @throws SAXException
+     * @throws IOException
+     */
+    public void testGetFileLinkPathForDeletedFile() throws IOException, SAXException {
+        final HashMap<String,Path> pathMap = createPathMap("rawchangelog-with-deleted-file");
+        final Path path = pathMap.get("bar");
+        final URL fileLink = redmineWeb.getFileLink(path);
+        assertEquals(REDMINE_URL + "/revisions/fc029da233f161c65eb06d0f1ed4f36ae81d1f4f/diff/bar", String.valueOf(fileLink));
+    }
+
+    private GitChangeSet createChangeSet(String rawchangelogpath) throws IOException, SAXException {
+        final File rawchangelog = new File(RedmineWebTest.class.getResource(rawchangelogpath).getFile());
+        final GitChangeLogParser logParser = new GitChangeLogParser(false);
+        final List<GitChangeSet> changeSetList = logParser.parse(null, rawchangelog).getLogs();
+        return changeSetList.get(0);
+    }
+
+    /**
+     * @param changelog
+     * @return
+     * @throws IOException
+     * @throws SAXException
+     */
+    private HashMap<String, Path> createPathMap(final String changelog) throws IOException, SAXException {
+        final HashMap<String, Path> pathMap = new HashMap<String, Path>();
+        final Collection<Path> changeSet = createChangeSet(changelog).getPaths();
+        for (final Path path : changeSet) {
+            pathMap.put(path.getPath(), path);
+        }
+        return pathMap;
+    }
+
+
+}
