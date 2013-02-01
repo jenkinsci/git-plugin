2c8a72d1ba75dfd5e78dc41d5850cd6b95d55bf7
Gabor Liptak <gliptak@gmail.com>
2013-02-01 11:25:34 -0500

    Add test for GitAPI.getTagNames() with remote

--- src/test/java/hudson/plugins/git/GitAPITest.java
+++ src/test/java/hudson/plugins/git/GitAPITest.java
@@ -1,13 +1,14 @@
 package hudson.plugins.git;
 
-import java.io.IOException;
-import java.util.Set;
-
-import org.jvnet.hudson.test.HudsonTestCase;
-
 import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.util.StreamTaskListener;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Set;
+
+import org.jvnet.hudson.test.HudsonTestCase;
 
 public class GitAPITest extends HudsonTestCase {
 
@@ -24,6 +25,27 @@ public class GitAPITest extends HudsonTestCase {
         moduleRepo.tag("foo1", "foo1 comment");
         moduleRepo.tag("bar", "bar comment");
         Set<String> names=moduleRepo.getTagNames("foo*");
+        assertEquals(2, names.size());
+	}
+
+	public void testGetTagNamesFromCloned() throws IOException, InterruptedException, URISyntaxException {
+		StreamTaskListener listener = StreamTaskListener.fromStderr();
+        FilePath moduleRemoteWs = new FilePath(createTmpDir());
+        GitAPI moduleRemoteRepo = new GitAPI("git", moduleRemoteWs, listener, new EnvVars());
+        moduleRemoteRepo.init();
+        FilePath fileA=moduleRemoteWs.child("a");
+        fileA.touch(0);
+        moduleRemoteRepo.add("a");
+        moduleRemoteRepo.launchCommand("commit", "-m", "Initial commit");
+        moduleRemoteRepo.tag("foo", "foo comment");
+        moduleRemoteRepo.tag("foo1", "foo1 comment");
+        moduleRemoteRepo.tag("bar", "bar comment");
+        // there must be an easier way to clone another file repo ...
+        FilePath moduleLocalWs = new FilePath(createTmpDir());
+        GitAPI tempRepo = new GitAPI("git", moduleLocalWs, listener, new EnvVars());
+        tempRepo.launchCommand("clone", "file://"+moduleRemoteWs.getRemote(), "jenkins-git");
+        GitAPI moduleLocalRepo = new GitAPI("git", moduleLocalWs.child("jenkins-git"), listener, new EnvVars());
+        Set<String> names=moduleLocalRepo.getTagNames("foo*");
         assertEquals(2, names.size());
 	}
 
