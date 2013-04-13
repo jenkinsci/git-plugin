jenkinsci/git-plugin

commit a7a9e3d714adc80eab8a9271c79883b6cdf7bd93

also expand the name being displayed in the logs

src/main/java/hudson/plugins/git/GitSCM.java
@@ -1173,7 +1173,7 @@ public class GitSCM extends SCM implements Serializable {
                     // us..
                     listener.getLogger().println(
                             "Merging " + revToBuild + " onto "
-                            + mergeOptions.getMergeTarget());
+                            + getParameterString(mergeOptions.getMergeTarget(), build));
 
                     // checkout origin/blah
                     ObjectId target = git.revParse(remoteBranchName);

commit 4a95b27543c8985adefbde2f78adb8760184ffad

[JENKINS-16941] Fix serialisation issue.
It reverts https://github.com/cosmin/git-plugin/commit/a7a9e3d714adc80eab8a9271c79883b6cdf7bd93 which caused the whole issue.

src/main/java/hudson/plugins/git/GitSCM.java
@@ -1143,7 +1143,7 @@ public class GitSCM extends SCM implements Serializable {
                     // us..
                     listener.getLogger().println(
                             "Merging " + revToBuild + " onto "
-                            + getParameterString(mergeOptions.getMergeTarget(), build));
+                            + mergeOptions.getMergeTarget());
 
                     // checkout origin/blah
                     ObjectId target = git.revParse(remoteBranchName);