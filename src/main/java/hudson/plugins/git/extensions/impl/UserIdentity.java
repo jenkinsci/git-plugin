4608d82d8ccaad6ed77082e5ed1a65712b0797d4
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-06-16 13:00:50 +0100

    Configure author/committer more reliably

--- src/main/java/hudson/plugins/git/extensions/impl/UserIdentity.java
+++ src/main/java/hudson/plugins/git/extensions/impl/UserIdentity.java
@@ -1,11 +1,14 @@
 package hudson.plugins.git.extensions.impl;
 
 import hudson.Extension;
+import hudson.plugins.git.GitException;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.extensions.GitSCMExtension;
 import hudson.plugins.git.extensions.GitSCMExtensionDescriptor;
+import org.jenkinsci.plugins.gitclient.GitClient;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static hudson.Util.*;
@@ -36,6 +39,7 @@ public class UserIdentity extends GitSCMExtension {
 
     @Override
     public void populateEnvironmentVariables(GitSCM scm, Map<String, String> env) {
+        // for backward compatibility, in case the user's shell script invokes Git inside
         if (name!=null) {
             env.put("GIT_COMMITTER_NAME", name);
             env.put("GIT_AUTHOR_NAME", name);
@@ -44,6 +48,22 @@ public class UserIdentity extends GitSCMExtension {
             env.put("GIT_COMMITTER_EMAIL", email);
             env.put("GIT_AUTHOR_EMAIL", email);
         }
+    }
+
+    @Override
+    public GitClient decorate(GitSCM scm, GitClient git) throws IOException, InterruptedException, GitException {
+        GitSCM.DescriptorImpl d = scm.getDescriptor();
+
+        String n = d.getGlobalConfigName();
+        if (name!=null) n = name;
+
+        String e = d.getGlobalConfigEmail();
+        if (email!=null) e = email;
+
+        git.setAuthor(n,e);
+        git.setCommitter(n,e);
+
+        return git;
     }
 
     @Extension
