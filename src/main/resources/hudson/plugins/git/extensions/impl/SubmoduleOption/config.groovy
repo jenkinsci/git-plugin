83e57e6ed4353bc9e86fa6f2cac066f5fb6e1ec1
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-06-21 06:19:21 -0700

    Yay! Got rid of the advanced configuration section.
    Every fringe use case is now implemented as GitSCMExtension

--- src/main/resources/hudson/plugins/git/extensions/impl/SubmoduleOption/config.groovy
+++ src/main/resources/hudson/plugins/git/extensions/impl/SubmoduleOption/config.groovy
@@ -8,3 +8,33 @@ f.entry(title:__("Disable submodules processing"), field:"disableSubmodules") {
 f.entry(title:__("Recursively update submodules"), field:"recursiveSubmodules") {
     f.checkbox()
 }
+
+/*
+  This needs more thought
+
+  <f:entry title="Autogenerate submodule configurations">
+    <f:checkbox name="git.generate" checked="${scm.doGenerate}"/>
+    <label class="attach-previous">Generate submodule configurations</label>
+
+    <f:repeatable var="smcfg" name="smcfg" varStatus="cfgStatus" items="${scm.submoduleCfg}" noAddButton="false">
+           <table width="100%">
+           <f:entry title="Name of submodule">
+             <f:textbox name="git.submodule.name" value="${smcfg.submoduleName}" />
+           </f:entry>
+
+           <f:entry title="Matching Branches">
+            <f:textbox name="git.submodule.match" value="${smcfg.branchesString}" />
+           </f:entry>
+
+
+           <f:entry>
+            <div align="right">
+                <input type="button" value="Delete" class="repeatable-delete" style="margin-left: 1em;" />
+            </div>
+          </f:entry>
+          </table>
+
+        </f:repeatable>
+
+  </f:entry>
+*/
