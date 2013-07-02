bb07f47476627f2d9957758c696fd7ed18cb45bb
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-07-01 21:42:05 -0700

    typo

--- src/main/resources/hudson/plugins/git/extensions/impl/CloneOption/config.groovy
+++ src/main/resources/hudson/plugins/git/extensions/impl/CloneOption/config.groovy
@@ -2,9 +2,9 @@ package hudson.plugins.git.extensions.impl.CloneOption;
 
 def f = namespace(lib.FormTagLib);
 
-f.entry(title:__("Shallow clone"), field:"shallow") {
+f.entry(title:_("Shallow clone"), field:"shallow") {
     f.checkbox()
 }
-f.entry(title:__("Path of the reference repo to use during clone"), field:"reference") {
+f.entry(title:_("Path of the reference repo to use during clone"), field:"reference") {
     f.textbox()
 }
--- src/main/resources/hudson/plugins/git/extensions/impl/SubmoduleOption/config.groovy
+++ src/main/resources/hudson/plugins/git/extensions/impl/SubmoduleOption/config.groovy
@@ -2,10 +2,10 @@ package hudson.plugins.git.extensions.impl.SubmoduleOption;
 
 def f = namespace(lib.FormTagLib);
 
-f.entry(title:__("Disable submodules processing"), field:"disableSubmodules") {
+f.entry(title:_("Disable submodules processing"), field:"disableSubmodules") {
     f.checkbox()
 }
-f.entry(title:__("Recursively update submodules"), field:"recursiveSubmodules") {
+f.entry(title:_("Recursively update submodules"), field:"recursiveSubmodules") {
     f.checkbox()
 }
 
