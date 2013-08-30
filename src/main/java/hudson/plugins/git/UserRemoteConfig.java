4bc2dda03d3511852ef897ed2fd9fcc81a370be4
Nicolas De Loof <nicolas.deloof@gmail.com>
2013-08-30 12:00:21 +0200

    UI to let user configure credentials for git repository

--- src/main/java/hudson/plugins/git/UserRemoteConfig.java
+++ src/main/java/hudson/plugins/git/UserRemoteConfig.java
@@ -1,19 +1,29 @@
 package hudson.plugins.git;
 
+import com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey;
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardCredentials;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.Util;
 import hudson.model.AbstractDescribableImpl;
+import hudson.model.AbstractProject;
 import hudson.model.Descriptor;
 import hudson.model.Item;
 import hudson.model.TaskListener;
+import hudson.security.ACL;
 import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 import org.apache.commons.lang.StringUtils;
-import org.jenkinsci.plugins.gitclient.CliGitAPIImpl;
-import hudson.plugins.git.GitTool;
 import org.jenkinsci.plugins.gitclient.Git;
 import org.jenkinsci.plugins.gitclient.GitClient;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.export.Exported;
@@ -21,6 +31,10 @@ import org.kohsuke.stapler.export.ExportedBean;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @ExportedBean
 public class UserRemoteConfig extends AbstractDescribableImpl<UserRemoteConfig> implements Serializable {
@@ -28,12 +42,14 @@ public class UserRemoteConfig extends AbstractDescribableImpl<UserRemoteConfig> 
     private String name;
     private String refspec;
     private String url;
+    private String credentialsId;
 
     @DataBoundConstructor
-    public UserRemoteConfig(String url, String name, String refspec) {
+    public UserRemoteConfig(String url, String name, String refspec, String credentialsId) {
         this.url = StringUtils.trim(url);
         this.name = name;
         this.refspec = refspec;
+        this.credentialsId = credentialsId;
     }
 
     @Exported
@@ -51,14 +67,48 @@ public class UserRemoteConfig extends AbstractDescribableImpl<UserRemoteConfig> 
         return url;
     }
 
+    @Exported
+    public String getCredentialsId() {
+        return credentialsId;
+    }
+
     public String toString() {
         return getRefspec() + " => " + getUrl() + " (" + getName() + ")";
     }
 
+    private final static Pattern SCP_LIKE = Pattern.compile("(.*):(.*)");
+
     @Extension
     public static class DescriptorImpl extends Descriptor<UserRemoteConfig> {
 
-        public FormValidation doCheckUrl(@QueryParameter String value) throws IOException, InterruptedException {
+        public ListBoxModel doFillCredentialsIdItems(@AncestorInPath AbstractProject project,
+                                                     @QueryParameter String url) {
+            List<DomainRequirement> domainRequirements;
+            if (StringUtils.isEmpty(url)) {
+                domainRequirements = Collections.<DomainRequirement>emptyList();
+            } else {
+                Matcher m = SCP_LIKE.matcher(url);
+                if (m.matches())
+                    url = "ssh://"+m.group(1)+'/'+m.group(2);
+                domainRequirements = URIRequirementBuilder.fromUri(url.trim()).build();
+            }
+            return new StandardListBoxModel()
+                    .withEmptySelection()
+                    .withMatching(
+                            CredentialsMatchers.anyOf(
+                                    CredentialsMatchers.instanceOf(StandardUsernamePasswordCredentials.class),
+                                    CredentialsMatchers.instanceOf(SSHUserPrivateKey.class)
+                            ),
+                            CredentialsProvider.lookupCredentials(StandardCredentials.class,
+                                    project,
+                                    ACL.SYSTEM,
+                                    domainRequirements)
+                    );
+        }
+
+        public FormValidation doCheckUrl(@AncestorInPath AbstractProject project,
+                                         @QueryParameter String credentialId,
+                                         @QueryParameter String value) throws IOException, InterruptedException {
 
             String url = Util.fixEmptyAndTrim(value);
             if (url == null)
@@ -77,6 +127,7 @@ public class UserRemoteConfig extends AbstractDescribableImpl<UserRemoteConfig> 
             GitClient git = Git.with(TaskListener.NULL, environment)
                     .using(GitTool.getDefaultInstallation().getGitExe())
                     .getClient();
+            git.setCredentials(lookupCredentials(project, credentialId));
 
             // attempt to connect the provided URL
             try {
@@ -86,6 +137,12 @@ public class UserRemoteConfig extends AbstractDescribableImpl<UserRemoteConfig> 
             }
 
             return FormValidation.ok();
+        }
+
+        private static StandardCredentials lookupCredentials(AbstractProject project, String credentialId) {
+            return (credentialId == null) ? null : CredentialsMatchers.firstOrNull(
+                        CredentialsProvider.lookupCredentials(StandardCredentials.class, project, ACL.SYSTEM, null),
+                        CredentialsMatchers.withId(credentialId));
         }
 
         @Override
