d98a86ce285eac05fbf1d14af443a9ff3bfba318
Nicolas De Loof <nicolas.deloof@gmail.com>
2013-10-15 11:50:00 +0200

    [FIXED JENKINS-19997] fix buildChooser selection

--- src/main/resources/hudson/plugins/git/extensions/impl/BuildChooserSetting/config.groovy
+++ src/main/resources/hudson/plugins/git/extensions/impl/BuildChooserSetting/config.groovy
@@ -2,6 +2,7 @@ package hudson.plugins.git.extensions.impl.BuildChooserSetting
 
 def f = namespace(lib.FormTagLib);
 
-f.entry(title:_("Choosing strategy"), field:"buildChooser") {
-    f.dropdownDescriptorSelector(descriptors: descriptor.getBuildChooserDescriptors(it))
+f.entry() {
+    f.dropdownDescriptorSelector(title:_("Choosing strategy"), field:"buildChooser",
+            descriptors: descriptor.getBuildChooserDescriptors(my))
 }
