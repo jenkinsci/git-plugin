jenkinsci/git-plugin

commit dd140153ecb7c01c7ff65c30cf4a2854502ce7ed

check SCMTrigger isIgnorePostCommitHook

src/main/java/hudson/plugins/git/GitStatus.java
@@ -188,11 +188,11 @@ public class GitStatus extends AbstractModelObject implements UnprotectedRootAct
          * @deprecated implement #onNotifyCommit(org.eclipse.jgit.transport.URIish, String, String...)
          */
         public List<ResponseContributor> onNotifyCommit(URIish uri, String[] branches) {
-            return Collections.EMPTY_LIST;
+            return onNotifyCommit(uri, null, branches);
         }
 
         public List<ResponseContributor> onNotifyCommit(URIish uri, @Nullable String sha1, String... branches) {
-            return onNotifyCommit(uri, branches);
+            return Collections.EMPTY_LIST;
         }
     }
 
@@ -241,30 +241,22 @@ public class GitStatus extends AbstractModelObject implements UnprotectedRootAct
                             }
 
                             if (branches.length == 0) {
-                                branchMatches = true;
+                                urlFound = true;
                             } else {
-                                for (BranchSpec branchSpec : git.getBranches()) {
+                                OUT: for (BranchSpec branchSpec : git.getBranches()) {
                                     for (String branch : branches) {
                                         if (branchSpec.matches(repository.getName() + "/" + branch)) {
-                                            branchMatches = true;
-                                            break;
+                                            urlFound = true;
+                                            break OUT;
                                         }
-                                    }
-                                    if (branchMatches) {
-                                        break;
                                     }
                                 }
                             }
 
-                            if (branchMatches) {
-                                urlFound = true;
-                            } else {
-                                continue;
-                            }
-
+                            if (!urlFound) continue;
 
                             SCMTrigger trigger = project.getTrigger(SCMTrigger.class);
-                            if (trigger != null) {
+                            if (trigger != null && !trigger.isIgnorePostCommitHooks()) {
                                 triggerFound = true;
                             } else {
                                 continue;

commit 53cc00e9108fa0a029c24b8de94188613c83be0e

Changes in dd140153 broke the notifier when trigger is null

src/main/java/hudson/plugins/git/GitStatus.java
@@ -240,7 +240,10 @@ public class GitStatus extends AbstractModelObject implements UnprotectedRootAct
                             }
 
                             SCMTrigger trigger = project.getTrigger(SCMTrigger.class);
-                            if (trigger != null && trigger.isIgnorePostCommitHooks()) {
+                            if (trigger == null) {
+                                continue;
+                            }
+                            if (trigger.isIgnorePostCommitHooks()) {
                                 LOGGER.info("PostCommitHooks are disabled on " + project.getFullDisplayName());
                                 continue;
                             }

src/test/java/hudson/plugins/git/GitStatusTest.java
@@ -123,7 +123,14 @@ public class GitStatusTest extends HudsonTestCase {
         Mockito.verify(aMasterTrigger, Mockito.never()).run();
     }
 
-    private SCMTrigger setupProject(String url, String branchString, boolean ignoreNotifyCommit) throws Exception {
+    public void testDoNotifyCommitWithNoScmTrigger() throws Exception {
+        setupProject("a", "master", null);
+
+        this.gitStatus.doNotifyCommit("a", null, "");
+        // no expectation here, however we shouldn't have a build triggered, and no exception
+    }
+
+    private SCMTrigger setupProject(String url, String branchString, SCMTrigger trigger) throws Exception {
         FreeStyleProject project = createFreeStyleProject();
         GitSCM git = new GitSCM(
                 Collections.singletonList(new UserRemoteConfig(url, null, null, null)),
@@ -132,10 +139,16 @@ public class GitStatusTest extends HudsonTestCase {
                 null, null,
                 Collections.<GitSCMExtension>emptyList());
         project.setScm(git);
+        if (trigger != null) {
+            project.addTrigger(trigger);
+        }
+        return trigger;
+    }
+
+    private SCMTrigger setupProject(String url, String branchString, boolean ignoreNotifyCommit) throws Exception {
         SCMTrigger trigger = Mockito.mock(SCMTrigger.class);
         Mockito.doReturn(ignoreNotifyCommit).when(trigger).isIgnorePostCommitHooks();
-        project.addTrigger(trigger);
-        return trigger;
+        return setupProject(url, branchString, trigger);
     }
 
     public void testLooseMatch() throws URISyntaxException {