268eec426ea40cdfc1932036f67a2054adae09db
Jesse Glick <jglick@cloudbees.com>
2014-06-16 15:05:11 -0400

    Updating Javadoc.

--- src/main/java/hudson/plugins/git/extensions/impl/ScmName.java
+++ src/main/java/hudson/plugins/git/extensions/impl/ScmName.java
@@ -1,13 +1,14 @@
 package hudson.plugins.git.extensions.impl;
 
 import hudson.Extension;
+import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.extensions.FakeGitSCMExtension;
 import hudson.plugins.git.extensions.GitSCMExtensionDescriptor;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * When used with {@code org.jenkinsci.plugins.multiplescms.MultiSCM}, this differentiates a different instance.
- *
+ * Not strictly necessary any more since {@link GitSCM#getKey} will compute a default value, but can improve visual appearance of multiple-SCM changelogs.
  * @author Kohsuke Kawaguchi
  */
 public class ScmName extends FakeGitSCMExtension {
