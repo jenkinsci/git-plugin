7fbd682693f143b628e16e5776d5e8e4b7590b6f
Elvys Borges <elvys.borges@gmail.com>
2014-07-03 17:23:53 -0300

    [FIXED JENKINS-23675] Expand parameters on repository url before associate one url to one credential

--- src/main/java/hudson/plugins/git/GitSCM.java
+++ src/main/java/hudson/plugins/git/GitSCM.java
@@ -570,6 +570,7 @@ public class GitSCM extends GitSCMBackwardCompatibility {
         for (UserRemoteConfig uc : getUserRemoteConfigs()) {
             if (uc.getCredentialsId() != null) {
                 String url = uc.getUrl();
+                url = getParameterString(url, environment);
                 StandardUsernameCredentials credentials = CredentialsMatchers
                         .firstOrNull(
                                 CredentialsProvider.lookupCredentials(StandardUsernameCredentials.class, project,
