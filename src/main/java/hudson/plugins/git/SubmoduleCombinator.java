4fb1492a5a98bf0598451e98bd5ab77bbb39dc09
Mark Waite <mark.earl.waite@gmail.com>
2014-08-10 16:10:21 -0600

    Remove unread field from SubmoduleCOmbinator

--- src/main/java/hudson/plugins/git/SubmoduleCombinator.java
+++ src/main/java/hudson/plugins/git/SubmoduleCombinator.java
@@ -19,7 +19,6 @@ import java.util.Map.Entry;
  */
 public class SubmoduleCombinator {
     GitClient git;
-    FilePath workspace;
     TaskListener listener;
 
     long         tid = new Date().getTime();
@@ -30,8 +29,6 @@ public class SubmoduleCombinator {
     public SubmoduleCombinator(GitClient git, TaskListener listener, Collection<SubmoduleConfig> cfg) {
         this.git = git;
         this.listener = listener;
-    
-        this.workspace = git.getWorkTree();
         this.submoduleConfig = cfg;
     }
 
