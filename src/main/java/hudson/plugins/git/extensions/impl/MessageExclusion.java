a68b8d902a9149d1d30ba6658679d68f66b77a29
Kanstantsin Shautsou <Kanstantsin_Shautsou@epam.com>
2014-10-24 18:23:07 +0300

    Added behavior: "Polling ignores commits with certain messages"

---
+++ src/main/java/hudson/plugins/git/extensions/impl/MessageExclusion.java
@@ -0,0 +1,53 @@
+package hudson.plugins.git.extensions.impl;
+
+import hudson.Extension;
+import hudson.model.TaskListener;
+import hudson.plugins.git.GitChangeSet;
+import hudson.plugins.git.GitException;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.extensions.GitSCMExtension;
+import hudson.plugins.git.extensions.GitSCMExtensionDescriptor;
+import hudson.plugins.git.util.BuildData;
+import org.jenkinsci.plugins.gitclient.GitClient;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+
+/**
+ * {@link GitSCMExtension} that ignores commits with specific messages.
+ *
+ * @author Kanstantsin Shautsou
+ */
+public class MessageExclusion extends GitSCMExtension {
+	/**
+	 * Java Pattern for matching messages to be ignored.
+	 */
+	private String excludedMessage;
+
+	@DataBoundConstructor
+	public MessageExclusion(String excludedMessage) { this.excludedMessage = excludedMessage; }
+
+	@Override
+	public boolean requiresWorkspaceForPolling() { return true; }
+
+	public String getExcludedMessage() { return excludedMessage; }
+
+	@Override
+	public Boolean isRevExcluded(GitSCM scm, GitClient git, GitChangeSet commit, TaskListener listener, BuildData buildData) throws IOException, InterruptedException, GitException {
+		String msg = commit.getMsg();
+		if (msg.matches(excludedMessage)){
+			listener.getLogger().println("Ignored commit " + commit.getId() + ": Found excluded message: " + msg);
+			return true;
+		}
+
+		return null;
+	}
+
+	@Extension
+	public static class DescriptorImpl extends GitSCMExtensionDescriptor {
+		@Override
+		public String getDisplayName() {
+			return "Polling ignores commits with certain messages";
+		}
+	}
+}
