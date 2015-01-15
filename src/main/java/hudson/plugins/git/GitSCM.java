ff5dcd95b1f8cd1d4c3f805bad43bf3794a999ae
Matthew Behlke <mbehlke@teslamotors.com>
2015-01-15 10:50:21 -0800

    Remove unnecessary comment

--- src/main/java/hudson/plugins/git/GitSCM.java
+++ src/main/java/hudson/plugins/git/GitSCM.java
@@ -1030,7 +1030,6 @@ public class GitSCM extends GitSCMBackwardCompatibility {
         
         // Print final summary
         listener.getLogger().println(summary);
-        // Clear summary for next build
         summary = "";
     }
 
