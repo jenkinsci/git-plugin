65a2b5f4926a0316880a567f16afd8dd4bac68e2
Mark Waite <mark.earl.waite@gmail.com>
2015-07-03 06:25:03 -0600

    Improve BuildDataTest speed and switch from JUnit 3 to JUnit 4

--- src/test/java/hudson/plugins/git/util/BuildDataTest.java
+++ src/test/java/hudson/plugins/git/util/BuildDataTest.java
@@ -1,24 +1,20 @@
 package hudson.plugins.git.util;
 
-import hudson.plugins.git.AbstractGitTestCase;
-
-import hudson.plugins.git.util.BuildData;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * @author Mark Waite
  */
-public class BuildDataTest extends AbstractGitTestCase {
-    /**
-     * Verifies that the display name is "Git Build Data".
-     */
+public class BuildDataTest {
+
+    @Test
     public void testDisplayNameWithoutSCM() throws Exception {
         final BuildData data = new BuildData();
         assertEquals(data.getDisplayName(), "Git Build Data");
     }
 
-    /**
-     * Verifies that the display name is "Git Build Data:scmName".
-     */
+    @Test
     public void testDisplayNameWithSCM() throws Exception {
         final String scmName = "testSCM";
         final BuildData data = new BuildData(scmName);
