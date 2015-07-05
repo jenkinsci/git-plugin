c28281747e97e790b38124af13f924599486a247
Mark Waite <mark.earl.waite@gmail.com>
2015-07-05 16:16:40 -0600

    Use AbstractGitRepository in CommitTimeComparatorTest & DefaultBuildChooserTest
    Switches the tests to JUnit 4.
    
    Nicely reduces test time by replacing inheritance from AbstractGitTestCase
    with inheritance from AbstractGitRepository. CommitTimeComparatorTest &
    DefaultBuildChooserTest need a local repository (AbstractGitRepository),
    not a complete Jenkins instance (AbstractGitTestCase).

--- src/test/java/hudson/plugins/git/util/DefaultBuildChooserTest.java
+++ src/test/java/hudson/plugins/git/util/DefaultBuildChooserTest.java
@@ -1,36 +1,40 @@
 package hudson.plugins.git.util;
 
+import hudson.plugins.git.AbstractGitRepository;
 import java.util.Collection;
 
-import hudson.plugins.git.AbstractGitTestCase;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.Revision;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * @author Arnout Engelen
  */
-public class DefaultBuildChooserTest extends AbstractGitTestCase {
+public class DefaultBuildChooserTest extends AbstractGitRepository {
+    @Test
     public void testChooseGitRevisionToBuildByShaHash() throws Exception {
-        git.commit("Commit 1");
-        String shaHashCommit1 = git.getBranches().iterator().next().getSHA1String();
-        git.commit("Commit 2");
-        String shaHashCommit2 = git.getBranches().iterator().next().getSHA1String();
+        testGitClient.commit("Commit 1");
+        String shaHashCommit1 = testGitClient.getBranches().iterator().next().getSHA1String();
+        testGitClient.commit("Commit 2");
+        String shaHashCommit2 = testGitClient.getBranches().iterator().next().getSHA1String();
         assertNotSame(shaHashCommit1, shaHashCommit2);
 
         DefaultBuildChooser buildChooser = (DefaultBuildChooser) new GitSCM("foo").getBuildChooser();
 
-        Collection<Revision> candidateRevisions = buildChooser.getCandidateRevisions(false, shaHashCommit1, git, null, null, null);
+        Collection<Revision> candidateRevisions = buildChooser.getCandidateRevisions(false, shaHashCommit1, testGitClient, null, null, null);
 
         assertEquals(1, candidateRevisions.size());
         assertEquals(shaHashCommit1, candidateRevisions.iterator().next().getSha1String());
 
-        candidateRevisions = buildChooser.getCandidateRevisions(false, "aaa" + shaHashCommit1.substring(3), git, null, null, null);
+        candidateRevisions = buildChooser.getCandidateRevisions(false, "aaa" + shaHashCommit1.substring(3), testGitClient, null, null, null);
         assertTrue(candidateRevisions.isEmpty());
     }
     /**
      * RegExp patterns prefixed with : should pass through to DefaultBuildChooser.getAdvancedCandidateRevisions
      * @throws Exception
      */
+    @Test
     public void testIsAdvancedSpec() throws Exception {
         DefaultBuildChooser buildChooser = (DefaultBuildChooser) new GitSCM("foo").getBuildChooser();
 
