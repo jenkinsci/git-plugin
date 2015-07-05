e6c1a7d4c01cdbad8bd420d17607482acf77b7d0
Mark Waite <mark.earl.waite@gmail.com>
2015-07-05 15:34:29 -0600

    Reduce test time for GitChangeSet tests - only 1 test needs a Jenkins instance
    Use a utility class to construct the changeset rather than duplicating
    the changeset creation code in multiple tests.

---
+++ src/test/java/hudson/plugins/git/GitChangeSetBasicTest.java
@@ -0,0 +1,103 @@
+package hudson.plugins.git;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GitChangeSetBasicTest {
+
+    private GitChangeSet genChangeSet(boolean authorOrCommitter, boolean useLegacyFormat) {
+        return GitChangeSetUtil.genChangeSet(authorOrCommitter, useLegacyFormat, true);
+    }
+
+    private GitChangeSet genChangeSet(boolean authorOrCommitter, boolean useLegacyFormat, boolean hasParent) {
+        return GitChangeSetUtil.genChangeSet(authorOrCommitter, useLegacyFormat, hasParent);
+    }
+
+    @Test
+    public void testLegacyChangeSet() {
+        GitChangeSetUtil.assertChangeSet(genChangeSet(false, true));
+    }
+
+    @Test
+    public void testChangeSet() {
+        GitChangeSetUtil.assertChangeSet(genChangeSet(false, false));
+    }
+
+    @Test
+    public void testChangeSetNoParent() {
+        GitChangeSet changeSet = genChangeSet(false, false, false);
+        GitChangeSetUtil.assertChangeSet(changeSet);
+        assertNull(changeSet.getParentCommit());
+    }
+
+    @Test
+    public void testCommitter() {
+        assertEquals(GitChangeSetUtil.COMMITTER_NAME, genChangeSet(false, false).getAuthorName());
+    }
+
+    @Test
+    public void testAuthor() {
+        assertEquals(GitChangeSetUtil.AUTHOR_NAME, genChangeSet(true, false).getAuthorName());
+    }
+
+    @Test
+    public void testIsoDate() {
+
+        GitChangeSet c = new GitChangeSet(Arrays.asList("author John Doe <john.doe@jenkins-ci.org> 2015-03-03T09:22:42-0700"), true);
+        assertEquals("2015-03-03T09:22:42-0700", c.getDate());
+        assertEquals(1425399762000L, c.getTimestamp());
+
+        c = new GitChangeSet(Arrays.asList("author John Doe <john.doe@jenkins-ci.org> 2015-03-03T09:22:42-07:00"), true);
+        assertEquals("2015-03-03T09:22:42-07:00", c.getDate());
+        assertEquals(1425399762000L, c.getTimestamp());
+
+        c = new GitChangeSet(Arrays.asList("author John Doe <john.doe@jenkins-ci.org> 2015-03-03T16:22:42Z"), true);
+        assertEquals("2015-03-03T16:22:42Z", c.getDate());
+        assertEquals(1425399762000L, c.getTimestamp());
+
+        c = new GitChangeSet(Arrays.asList("author John Doe <john.doe@jenkins-ci.org> 1425399762"), true);
+        assertEquals("2015-03-03T16:22:42Z", c.getDate());
+        assertEquals(1425399762000L, c.getTimestamp());
+
+        c = new GitChangeSet(Arrays.asList("author John Doe <john.doe@jenkins-ci.org> 1425374562 -0700"), true);
+        assertEquals("2015-03-03T09:22:42-0700", c.getDate());
+        assertEquals(1425399762000L, c.getTimestamp());
+    }
+
+    private GitChangeSet genChangeSetForSwedCase(boolean authorOrCommitter) {
+        ArrayList<String> lines = new ArrayList<String>();
+        lines.add("commit 1567861636cd854f4dd6fa40bf94c0c657681dd5");
+        lines.add("tree 66236cf9a1ac0c589172b450ed01f019a5697c49");
+        lines.add("parent e74a24e995305bd67a180f0ebc57927e2b8783ce");
+        lines.add("author misterÅ <mister.ahlander@ericsson.com> 1363879004 +0100");
+        lines.add("committer Mister Åhlander <mister.ahlander@ericsson.com> 1364199539 -0400");
+        lines.add("");
+        lines.add("    [task] Updated version.");
+        lines.add("    ");
+        lines.add("    Including earlier updates.");
+        lines.add("    ");
+        lines.add("    Changes in this version:");
+        lines.add("    - Changed to take the gerrit url from gerrit query command.");
+        lines.add("    - Aligned reason information with our new commit hooks");
+        lines.add("    ");
+        lines.add("    Change-Id: Ife96d2abed5b066d9620034bec5f04cf74b8c66d");
+        lines.add("    Reviewed-on: https://gerrit.e.se/12345");
+        lines.add("    Tested-by: Jenkins <jenkins@no-mail.com>");
+        lines.add("    Reviewed-by: Mister Another <mister.another@ericsson.com>");
+        lines.add("");
+        //above lines all on purpose vs specific troublesome case @ericsson.
+        return new GitChangeSet(lines, authorOrCommitter);
+    }
+
+    @Test
+    public void testSwedishCommitterName() {
+        assertEquals("Mister Åhlander", genChangeSetForSwedCase(false).getAuthorName());
+    }
+
+    @Test
+    public void testSwedishAuthorName() {
+        assertEquals("misterÅ", genChangeSetForSwedCase(true).getAuthorName());
+    }
+}
