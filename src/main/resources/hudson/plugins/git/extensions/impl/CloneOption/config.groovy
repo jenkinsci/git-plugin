83a1f9cb5a290ad36cc6b4fece14e2f83de0d977
Eduardo Aranega <aranega@yahoo-inc.com>
2015-07-22 17:33:10 -0700

    by default set the shallow clone checkbox

--- src/main/resources/hudson/plugins/git/extensions/impl/CloneOption/config.groovy
+++ src/main/resources/hudson/plugins/git/extensions/impl/CloneOption/config.groovy
@@ -3,7 +3,7 @@ package hudson.plugins.git.extensions.impl.CloneOption;
 def f = namespace(lib.FormTagLib);
 
 f.entry(title:_("Shallow clone"), field:"shallow") {
-    f.checkbox()
+    f.checkbox(default: true)
 }
 f.entry(title:_("Path of the reference repo to use during clone"), field:"reference") {
     f.textbox()
