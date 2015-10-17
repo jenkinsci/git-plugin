cd52d0e86cce1b93de3e07a7c891c9f0c4f6bb52
Mark Waite <Mark.Waite@ca.com>
2015-10-16 18:47:41 -0600

    First try at joda time based solution

--- src/main/java/hudson/plugins/git/GitChangeSet.java
+++ src/main/java/hudson/plugins/git/GitChangeSet.java
@@ -28,9 +28,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static hudson.Util.fixEmpty;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.ISODateTimeFormat;
 
 /**
  * Represents a change set.
+ *
  * @author Nigel Magnay
  */
 public class GitChangeSet extends ChangeLogSet.Entry {
@@ -54,8 +59,8 @@ public class GitChangeSet extends ChangeLogSet.Entry {
     // Look like '2015-09-30 08:21:24 -0600'
     // ISO is    '2015-09-30T08:21:24-06:00'
     private static final Pattern NEARLY_ISO_DATE = Pattern.compile("^([0-9]{4}-[0-9]{2}-[0-9]{2})" + // YYYY-MM-DD
-                                                                   " ([0-9]{2}:[0-9]{2}:[0-9]{2})" + // HH:mm:SS
-                                                                   " ([+-][0-9]{2})([0-9]{2})$");    // +-OFFSET
+            " ([0-9]{2}:[0-9]{2}:[0-9]{2})" + // HH:mm:SS
+            " ([+-][0-9]{2})([0-9]{2})$");    // +-OFFSET
 
     public static final Logger LOGGER = Logger.getLogger(GitChangeSet.class.getName());
 
@@ -63,20 +68,24 @@ public class GitChangeSet extends ChangeLogSet.Entry {
      * This is broken as a part of the 1.5 refactoring.
      *
      * <p>
-     * When we build a commit that multiple branches point to, Git plugin historically recorded
-     * changelogs "revOfBranchInPreviousBuild...revToBuild" for each branch separately. This
-     * however fails to take full generality of Git commit graph into account, as such rev-lists
-     * can share common commits, which then get reported multiple times.
+     * When we build a commit that multiple branches point to, Git plugin
+     * historically recorded changelogs
+     * "revOfBranchInPreviousBuild...revToBuild" for each branch separately.
+     * This however fails to take full generality of Git commit graph into
+     * account, as such rev-lists can share common commits, which then get
+     * reported multiple times.
      *
      * <p>
-     * In Git, a commit doesn't belong to a branch, in the sense that you cannot look at the object graph
-     * and re-construct exactly how branch has evolved. In that sense, trying to attribute commits to
-     * branches is a somewhat futile exercise.
+     * In Git, a commit doesn't belong to a branch, in the sense that you cannot
+     * look at the object graph and re-construct exactly how branch has evolved.
+     * In that sense, trying to attribute commits to branches is a somewhat
+     * futile exercise.
      *
      * <p>
-     * On the other hand, if this is still deemed important, the right thing to do is to traverse
-     * the commit graph and see if a commit can be only reachable from the "revOfBranchInPreviousBuild" of
-     * just one branch, in which case it's safe to attribute the commit to that branch.
+     * On the other hand, if this is still deemed important, the right thing to
+     * do is to traverse the commit graph and see if a commit can be only
+     * reachable from the "revOfBranchInPreviousBuild" of just one branch, in
+     * which case it's safe to attribute the commit to that branch.
      */
     private String committer;
     private String committerEmail;
@@ -109,17 +118,23 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         StringBuilder message = new StringBuilder();
 
         for (String line : lines) {
-            if( line.length() < 1)
+            if (line.length() < 1) {
                 continue;
+            }
             if (line.startsWith("commit ")) {
                 String[] split = line.split(" ");
-                if (split.length > 1) this.id = split[1];
-                else throw new IllegalArgumentException("Commit has no ID" + lines);
+                if (split.length > 1) {
+                    this.id = split[1];
+                } else {
+                    throw new IllegalArgumentException("Commit has no ID" + lines);
+                }
             } else if (line.startsWith("tree ")) {
             } else if (line.startsWith("parent ")) {
                 String[] split = line.split(" ");
                 // parent may be null for initial commit or changelog computed from a shallow clone
-                if (split.length > 1) this.parentCommit = split[1];
+                if (split.length > 1) {
+                    this.parentCommit = split[1];
+                }
             } else if (line.startsWith(PREFIX_COMMITTER)) {
                 Matcher committerMatcher = COMMITTER_ENTRY.matcher(line);
                 if (committerMatcher.matches()
@@ -147,7 +162,7 @@ public class GitChangeSet extends ChangeLogSet.Entry {
                         String path = fileMatcher.group(4);
                         char editMode = mode.charAt(0);
                         if (editMode == 'M' || editMode == 'A' || editMode == 'D'
-                            || editMode == 'R' || editMode == 'C') {
+                                || editMode == 'R' || editMode == 'C') {
                             src = parseHash(fileMatcher.group(1));
                             dst = parseHash(fileMatcher.group(2));
                         }
@@ -161,16 +176,14 @@ public class GitChangeSet extends ChangeLogSet.Entry {
                                 this.paths.add(new Path(src, dst, 'D', oldPath, this));
                                 this.paths.add(new Path(src, dst, 'A', newPath, this));
                             }
-                        }
-                        // Handle copy as an add
+                        } // Handle copy as an add
                         else if (editMode == 'C') {
                             Matcher copySplitMatcher = RENAME_SPLIT.matcher(path);
                             if (copySplitMatcher.matches() && copySplitMatcher.groupCount() >= 2) {
                                 String newPath = copySplitMatcher.group(2);
                                 this.paths.add(new Path(src, dst, 'A', newPath, this));
                             }
-                        }
-                        else {
+                        } else {
                             this.paths.add(new Path(src, dst, editMode, path, this));
                         }
                     }
@@ -188,14 +201,16 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         }
     }
 
-    /** Convert to iso date format if required */
+    /**
+     * Convert to iso date format if required
+     */
     private String isoDateFormat(String s) {
         String date = s;
         String timezone = "Z";
         int spaceIndex = s.indexOf(' ');
         if (spaceIndex > 0) {
             date = s.substring(0, spaceIndex);
-            timezone = s.substring(spaceIndex+1);
+            timezone = s.substring(spaceIndex + 1);
         }
         if (NumberUtils.isDigits(date)) {
             // legacy mode
@@ -228,6 +243,13 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         if (date.isEmpty()) {
             LOGGER.log(Level.WARNING, "Failed to parse empty date");
             return -1;
+        }
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss Z");
+        try {
+            DateTime dateTime = DateTime.parse(date, formatter);
+            return dateTime.getMillis();
+        } catch (IllegalArgumentException ie) {
+            LOGGER.log(Level.WARNING, "Failed to parse date: {0}", date);
         }
         Matcher nearlyISO = NEARLY_ISO_DATE.matcher(date);
         if (nearlyISO.matches()) {
@@ -269,7 +291,6 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         return parentCommit;
     }
 
-
     @Override
     public Collection<String> getAffectedPaths() {
         Collection<String> affectedPaths = new HashSet<String>(this.paths.size());
@@ -281,8 +302,8 @@ public class GitChangeSet extends ChangeLogSet.Entry {
 
     /**
      * Gets the files that are changed in this commit.
-     * @return
-     *      can be empty but never null.
+     *
+     * @return can be empty but never null.
      */
     @Exported
     public Collection<Path> getPaths() {
@@ -299,7 +320,8 @@ public class GitChangeSet extends ChangeLogSet.Entry {
      *
      * @param csAuthor user name.
      * @param csAuthorEmail user email.
-     * @param createAccountBasedOnEmail true if create new user based on committer's email.
+     * @param createAccountBasedOnEmail true if create new user based on
+     * committer's email.
      * @return {@link User}
      */
     public User findOrCreateUser(String csAuthor, String csAuthorEmail, boolean createAccountBasedOnEmail) {
@@ -314,8 +336,9 @@ public class GitChangeSet extends ChangeLogSet.Entry {
                 try {
                     user = User.get(csAuthorEmail, true);
                     user.setFullName(csAuthor);
-                    if (hasHudsonTasksMailer())
+                    if (hasHudsonTasksMailer()) {
                         setMail(user, csAuthorEmail);
+                    }
                     user.save();
                 } catch (IOException e) {
                     // add logging statement?
@@ -324,8 +347,9 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         } else {
             user = User.get(csAuthor, false);
 
-            if (user == null)
+            if (user == null) {
                 user = User.get(csAuthorEmail.split("@")[0], true);
+            }
         }
         // set email address for user if none is already available
         if (fixEmpty(csAuthorEmail) != null && hasHudsonTasksMailer() && !hasMail(user)) {
@@ -345,7 +369,7 @@ public class GitChangeSet extends ChangeLogSet.Entry {
     private boolean hasMail(User user) {
         hudson.tasks.Mailer.UserProperty property = user.getProperty(hudson.tasks.Mailer.UserProperty.class);
         return property != null && property.hasExplicitlyConfiguredAddress();
-	}
+    }
 
     private boolean hasHudsonTasksMailer() {
         // TODO convert to checking for mailer plugin as plugin migrates to 1.509+
@@ -377,8 +401,7 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         if (authorOrCommitter) {
             csAuthor = this.author;
             csAuthorEmail = this.authorEmail;
-        }
-        else {
+        } else {
             csAuthor = this.committer;
             csAuthorEmail = this.committerEmail;
         }
@@ -388,8 +411,8 @@ public class GitChangeSet extends ChangeLogSet.Entry {
 
     /**
      * Gets the author name for this changeset - note that this is mainly here
-     * so that we can test authorOrCommitter without needing a fully instantiated
-     * Hudson (which is needed for User.get in getAuthor()).
+     * so that we can test authorOrCommitter without needing a fully
+     * instantiated Hudson (which is needed for User.get in getAuthor()).
      *
      * @return author name
      */
@@ -420,12 +443,14 @@ public class GitChangeSet extends ChangeLogSet.Entry {
     }
 
     /**
-     * Gets {@linkplain #getComment() the comment} fully marked up by {@link ChangeLogAnnotator}.
+     * Gets {@linkplain #getComment() the comment} fully marked up by
+     * {@link ChangeLogAnnotator}.
      */
     public String getCommentAnnotated() {
         MarkupText markup = new MarkupText(getComment());
-        for (ChangeLogAnnotator a : ChangeLogAnnotator.all())
+        for (ChangeLogAnnotator a : ChangeLogAnnotator.all()) {
             a.annotate(getParent().getRun(), this, markup);
+        }
 
         return markup.toString(false);
     }
@@ -434,7 +459,7 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         return null;
     }
 
-    @ExportedBean(defaultVisibility=999)
+    @ExportedBean(defaultVisibility = 999)
     public static class Path implements AffectedFile {
 
         private String src;
@@ -459,7 +484,7 @@ public class GitChangeSet extends ChangeLogSet.Entry {
             return dst;
         }
 
-        @Exported(name="file")
+        @Exported(name = "file")
         public String getPath() {
             return path;
         }
@@ -471,12 +496,12 @@ public class GitChangeSet extends ChangeLogSet.Entry {
         @Exported
         public EditType getEditType() {
             switch (action) {
-            case 'A':
-                return EditType.ADD;
-            case 'D':
-                return EditType.DELETE;
-            default:
-                return EditType.EDIT;
+                case 'A':
+                    return EditType.ADD;
+                case 'D':
+                    return EditType.DELETE;
+                default:
+                    return EditType.EDIT;
             }
         }
     }
@@ -486,10 +511,12 @@ public class GitChangeSet extends ChangeLogSet.Entry {
     }
 
     public boolean equals(Object obj) {
-        if (obj == this)
+        if (obj == this) {
             return true;
-        if (obj instanceof GitChangeSet)
+        }
+        if (obj instanceof GitChangeSet) {
             return id != null && id.equals(((GitChangeSet) obj).id);
+        }
         return false;
     }
 }
