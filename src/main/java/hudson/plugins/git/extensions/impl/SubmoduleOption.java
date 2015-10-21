563b03f23b9b69e838b7482f9bcbf2635debac8b
Jacob Keller <jacob.e.keller@intel.com>
2015-10-21 13:51:40 -0700

    git-plugin: support use of parent credentials in submodules
    Extend the advanced submodules behavior plugin to support additional
    change of enabling "submodulesUseParentCreds" feature. This is a new
    feature that enables the simple method of allowing submodules to use
    their parent repository credentials instead of having to provide
    individual credentials per each submodule URL.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>

--- src/main/java/hudson/plugins/git/extensions/impl/SubmoduleOption.java
+++ src/main/java/hudson/plugins/git/extensions/impl/SubmoduleOption.java
@@ -39,13 +39,15 @@ public class SubmoduleOption extends GitSCMExtension {
     private boolean disableSubmodules;
     private boolean recursiveSubmodules;
     private boolean trackingSubmodules;
+    private boolean parentCredentials;
     private Integer timeout;
 
     @DataBoundConstructor
-    public SubmoduleOption(boolean disableSubmodules, boolean recursiveSubmodules, boolean trackingSubmodules, Integer timeout) {
+    public SubmoduleOption(boolean disableSubmodules, boolean recursiveSubmodules, boolean trackingSubmodules, boolean parentCredentials, Integer timeout) {
         this.disableSubmodules = disableSubmodules;
         this.recursiveSubmodules = recursiveSubmodules;
         this.trackingSubmodules = trackingSubmodules;
+        this.parentCredentials = parentCredentials;
         this.timeout = timeout;
     }
 
@@ -59,6 +61,10 @@ public class SubmoduleOption extends GitSCMExtension {
 
     public boolean isTrackingSubmodules() {
         return trackingSubmodules;
+    }
+
+    public boolean isParentCredentials() {
+        return parentCredentials;
     }
 
     public Integer getTimeout() {
@@ -83,6 +89,7 @@ public class SubmoduleOption extends GitSCMExtension {
             git.submoduleUpdate()
                 .recursive(recursiveSubmodules)
                 .remoteTracking(trackingSubmodules)
+                .parentCredentials(parentCredentials)
                 .timeout(timeout)
                 .execute();
         }
