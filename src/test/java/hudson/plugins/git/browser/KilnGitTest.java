7313876f5b039e2ab5f91a888eb205afffa37ce6
kwhetstone <kwhetstone@cloudbees.com>
2015-10-28 10:30:08 -0400

    Update KilnGitTest.java

--- src/test/java/hudson/plugins/git/browser/KilnGitTest.java
+++ src/test/java/hudson/plugins/git/browser/KilnGitTest.java
@@ -100,7 +100,7 @@ public class KilnGitTest {
     }
 
     private GitChangeSet createChangeSet(String rawchangelogpath) throws IOException, SAXException {
-        final File rawchangelog = new File(URLDecoder.decode(KilnGitTest.class.getResource(rawchangelogpath).getFile()), "utf-8");
+        final File rawchangelog = new File(URLDecoder.decode(KilnGitTest.class.getResource(rawchangelogpath).getFile(), "utf-8"));
         final GitChangeLogParser logParser = new GitChangeLogParser(false);
         final List<GitChangeSet> changeSetList = logParser.parse((Run) null, null, rawchangelog).getLogs();
         return changeSetList.get(0);
