14dd04970bc766c6ba7db16d3e61b9626ca0b44b
Robin Müller <coder-hugo@users.noreply.github.com>
2015-11-18 22:28:37 +0100

    Override the right onNotifyCommit method of the GitStatus.Listener
    - doNotifyCommit calls the onNotifyCommit method with the "full" signature which returns a EMPTY_LIST by default
      -> this method has to be overwritten by all listener implementations
      -> make this method abstract in the GitStatus.Listener class to ensure it gets implemented by all its subclasses

--- src/main/java/hudson/plugins/git/GitStatus.java
+++ src/main/java/hudson/plugins/git/GitStatus.java
@@ -120,7 +120,7 @@ public class GitStatus extends AbstractModelObject implements UnprotectedRootAct
     }
 
     /**
-     * Contributes to a {@link #doNotifyCommit(String, String, String)} response.
+     * Contributes to a {@link #doNotifyCommit(HttpServletRequest, String, String, String)} response.
      *
      * @since 1.4.1
      */
@@ -182,9 +182,7 @@ public class GitStatus extends AbstractModelObject implements UnprotectedRootAct
             return onNotifyCommit(uri, sha1, buildParameters, branches);
         }
 
-        public List<ResponseContributor> onNotifyCommit(URIish uri, @Nullable String sha1, List<ParameterValue> buildParameters, String... branches) {
-            return Collections.EMPTY_LIST;
-        }
+        public abstract List<ResponseContributor> onNotifyCommit(URIish uri, @Nullable String sha1, List<ParameterValue> buildParameters, String... branches);
 
 
     }
