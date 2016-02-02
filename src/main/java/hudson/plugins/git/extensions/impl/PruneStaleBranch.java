jenkinsci/git-plugin

commit c1872d038cc2f584bafbc375744deb54a1dbe316

[Fix JENKINS-29482] remove branches in BuildData history which doesn’t exist on remote repo (anymore)

src/main/java/hudson/plugins/git/extensions/impl/PruneStaleBranch.java
@@ -1,11 +1,13 @@
 package hudson.plugins.git.extensions.impl;
 
 import hudson.Extension;
+import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.plugins.git.GitException;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.extensions.GitSCMExtension;
 import hudson.plugins.git.extensions.GitSCMExtensionDescriptor;
+import hudson.plugins.git.util.BuildData;
 import org.jenkinsci.plugins.gitclient.FetchCommand;
 import org.jenkinsci.plugins.gitclient.GitClient;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -27,6 +29,12 @@ public class PruneStaleBranch extends GitSCMExtension {
     public void decorateFetchCommand(GitSCM scm, GitClient git, TaskListener listener, FetchCommand cmd) throws IOException, InterruptedException, GitException {
         listener.getLogger().println("Pruning obsolete local branches");
         cmd.prune();
+    }
+
+    @Override
+    public void onCheckoutCompleted(GitSCM scm, Run<?, ?> build, GitClient git, TaskListener listener) throws IOException, InterruptedException, GitException {
+        build.getAction(BuildData.class).purgeStaleBranches(git.getBranches());
+
     }
 
     @Extension

src/main/java/hudson/plugins/git/util/BuildData.java
@@ -244,4 +244,24 @@ public class BuildData implements Action, Serializable, Cloneable {
                 ",buildsByBranchName="+buildsByBranchName+
                 ",lastBuild="+lastBuild+"]";
     }
+
+    /**
+     * Remove branches from BuildData that have been seen in the past but do not exist anymore
+     * @param keepBranches all branches available in current repository state
+     */
+    public void purgeStaleBranches(Set<Branch> keepBranches) {
+        Set<String> names = new HashSet<String>(buildsByBranchName.keySet());
+        for (Branch branch : keepBranches) {
+            String name = branch.getName();
+            if (name.startsWith("refs/")) {
+                names.remove(name.substring(5));
+            }
+            if (name.startsWith("remotes/")) {
+                names.remove(name.substring(8));
+            }
+        }
+        for (String name : names) {
+            buildsByBranchName.remove(name);
+        }
+    }
 }

commit 336c1787c7b967e4220c4d459092397fda29892a

[Fix JENKINS-29482] Don't prune stale branches from BuildData
This reverts commit c1872d038cc2f584bafbc375744deb54a1dbe316.

Git plugin 2.4.1 included c1872d0 which regressed the plugin and
introduced JENKINS-32218

src/main/java/hudson/plugins/git/extensions/impl/PruneStaleBranch.java
@@ -1,13 +1,11 @@
 package hudson.plugins.git.extensions.impl;
 
 import hudson.Extension;
-import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.plugins.git.GitException;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.extensions.GitSCMExtension;
 import hudson.plugins.git.extensions.GitSCMExtensionDescriptor;
-import hudson.plugins.git.util.BuildData;
 import org.jenkinsci.plugins.gitclient.FetchCommand;
 import org.jenkinsci.plugins.gitclient.GitClient;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -29,12 +27,6 @@ public class PruneStaleBranch extends GitSCMExtension {
     public void decorateFetchCommand(GitSCM scm, GitClient git, TaskListener listener, FetchCommand cmd) throws IOException, InterruptedException, GitException {
         listener.getLogger().println("Pruning obsolete local branches");
         cmd.prune();
-    }
-
-    @Override
-    public void onCheckoutCompleted(GitSCM scm, Run<?, ?> build, GitClient git, TaskListener listener) throws IOException, InterruptedException, GitException {
-        build.getAction(BuildData.class).purgeStaleBranches(git.getBranches());
-
     }
 
     @Extension

src/main/java/hudson/plugins/git/util/BuildData.java
@@ -32,7 +32,7 @@ public class BuildData implements Action, Serializable, Cloneable {
      *
      * <p>
      * This map contains all the branches we've built in the past (including the build that this {@link BuildData}
-     * is attached to) 
+     * is attached to)
      */
     public Map<String, Build> buildsByBranchName = new HashMap<String, Build>();
 
@@ -90,7 +90,7 @@ public class BuildData implements Action, Serializable, Cloneable {
 
     public Object readResolve() {
         Map<String,Build> newBuildsByBranchName = new HashMap<String,Build>();
-        
+
         for (Map.Entry<String, Build> buildByBranchName : buildsByBranchName.entrySet()) {
             String branchName = fixNull(buildByBranchName.getKey());
             Build build = buildByBranchName.getValue();
@@ -104,7 +104,7 @@ public class BuildData implements Action, Serializable, Cloneable {
 
         return this;
     }
-    
+
     /**
      * Return true if the history shows this SHA1 has been built.
      * False otherwise.
@@ -147,7 +147,7 @@ public class BuildData implements Action, Serializable, Cloneable {
 
     /**
      * Gets revision of the previous build.
-     * @return revision of the last build. 
+     * @return revision of the last build.
      *    May be null will be returned if nothing has been checked out (e.g. due to wrong repository or branch)
      */
     @Exported
@@ -242,42 +242,5 @@ public class BuildData implements Action, Serializable, Cloneable {
                 ",remoteUrls="+remoteUrls+
                 ",buildsByBranchName="+buildsByBranchName+
                 ",lastBuild="+lastBuild+"]";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof BuildData)) {
-            return false;
-        } else {
-            BuildData otherBuildData = (BuildData) o;
-
-            if (otherBuildData.remoteUrls.equals(this.remoteUrls)
-                    && otherBuildData.buildsByBranchName.equals(this.buildsByBranchName)
-                    && otherBuildData.lastBuild.equals(this.lastBuild)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    /**
-     * Remove branches from BuildData that have been seen in the past but do not exist anymore
-     * @param keepBranches all branches available in current repository state
-     */
-    public void purgeStaleBranches(Set<Branch> keepBranches) {
-        Set<String> names = new HashSet<String>(buildsByBranchName.keySet());
-        for (Branch branch : keepBranches) {
-            String name = branch.getName();
-            if (name.startsWith("refs/")) {
-                names.remove(name.substring(5));
-            }
-            if (name.startsWith("remotes/")) {
-                names.remove(name.substring(8));
-            }
-        }
-        for (String name : names) {
-            buildsByBranchName.remove(name);
-        }
     }
 }