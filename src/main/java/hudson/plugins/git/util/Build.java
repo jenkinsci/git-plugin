0b32e07e301cd958bb38c9f2426736fd578fde6c
Mark Waite <mark.earl.waite@gmail.com>
2016-03-04 19:29:36 -0700

    Add Build.hashCode() to match Build.equals()

--- src/main/java/hudson/plugins/git/util/Build.java
+++ src/main/java/hudson/plugins/git/util/Build.java
@@ -109,6 +109,19 @@ public class Build implements Serializable, Cloneable {
     }
 
     @Override
+    public int hashCode() {
+        int result = 17;
+        result = 37 * result + this.hudsonBuildNumber;
+        if (this.revision != null) {
+            result = 37 * result + this.revision.hashCode();
+        }
+        if (this.marked != null) {
+            result = 37 * result + this.marked.hashCode();
+        }
+        return result;
+    }
+
+    @Override
     public Build clone() {
         Build clone;
         try {
