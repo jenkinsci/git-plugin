a303dc0d0beadb89cab34a92eaaf106652b380a4
Mark Waite <mark.earl.waite@gmail.com>
2016-07-02 09:16:06 -0600

    Limit numeric input fields to numeric values

--- src/main/resources/hudson/plugins/git/extensions/impl/CheckoutOption/config.groovy
+++ src/main/resources/hudson/plugins/git/extensions/impl/CheckoutOption/config.groovy
@@ -3,5 +3,5 @@ package hudson.plugins.git.extensions.impl.CheckoutOption;
 def f = namespace(lib.FormTagLib);
 
 f.entry(title:_("Timeout (in minutes) for checkout operation"), field:"timeout") {
-    f.textbox()
+    f.number(clazz:"number", min:1, step:1)
 }
