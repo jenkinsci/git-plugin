d1524c31df9c0b7c8fd5cfb5b5e18f99de16ea66
Mark Waite <mark.earl.waite@gmail.com>
2016-08-27 13:56:28 -0600

    Move basicTestIsExcluded into trivial tests

--- src/test/java/jenkins/plugins/git/AbstractGitSCMSourceTest.java
+++ src/test/java/jenkins/plugins/git/AbstractGitSCMSourceTest.java
@@ -8,12 +8,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.WithoutJenkins;
-import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
 
 /**
  * Tests for {@link AbstractGitSCMSource}
@@ -25,38 +21,6 @@ public class AbstractGitSCMSourceTest {
     @Rule
     public GitSampleRepoRule sampleRepo = new GitSampleRepoRule();
   
-  /*
-   * Test excluded branches
-   * TODO seems to overlap AbstractGitSCMSourceTrivialTest.testIsExcluded
-   */
-  @WithoutJenkins // unnecessary if moved into, say, AbstractGitSCMSourceTrivialTest
-  @Test
-  public void basicTestIsExcluded(){
-    AbstractGitSCMSource abstractGitSCMSource = mock(AbstractGitSCMSource.class);
-    
-    when(abstractGitSCMSource.getIncludes()).thenReturn("*master release* fe?ture");
-    when(abstractGitSCMSource.getExcludes()).thenReturn("release bugfix*");
-    when(abstractGitSCMSource.isExcluded(Mockito.anyString())).thenCallRealMethod();
-    
-    assertFalse(abstractGitSCMSource.isExcluded("master"));
-    assertFalse(abstractGitSCMSource.isExcluded("remote/master"));
-    assertFalse(abstractGitSCMSource.isExcluded("release/X.Y"));
-    assertFalse(abstractGitSCMSource.isExcluded("releaseX.Y"));
-    assertFalse(abstractGitSCMSource.isExcluded("fe?ture"));
-    assertTrue(abstractGitSCMSource.isExcluded("feature"));
-    assertTrue(abstractGitSCMSource.isExcluded("release"));
-    assertTrue(abstractGitSCMSource.isExcluded("bugfix"));
-    assertTrue(abstractGitSCMSource.isExcluded("bugfix/test"));
-    assertTrue(abstractGitSCMSource.isExcluded("test"));
-
-    when(abstractGitSCMSource.getIncludes()).thenReturn("master feature/*");
-    when(abstractGitSCMSource.getExcludes()).thenReturn("feature/*/private");
-    assertFalse(abstractGitSCMSource.isExcluded("master"));
-    assertTrue(abstractGitSCMSource.isExcluded("devel"));
-    assertFalse(abstractGitSCMSource.isExcluded("feature/spiffy"));
-    assertTrue(abstractGitSCMSource.isExcluded("feature/spiffy/private"));
-  }
-
     // TODO AbstractGitSCMSourceRetrieveHeadsTest *sounds* like it would be the right place, but it does not in fact retrieve any heads!
     @Issue("JENKINS-37482")
     @Test
