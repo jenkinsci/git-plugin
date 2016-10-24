1c20b7631aea3b0e7cf1c9e7033701bc9b280417
Jesse Glick <jglick@cloudbees.com>
2016-10-24 11:23:31 -0400

    Pending resolution of https://github.com/jenkinsci/credentials-plugin/pull/68, suppress FindBugs warnings.

--- src/main/java/hudson/plugins/git/UserRemoteConfig.java
+++ src/main/java/hudson/plugins/git/UserRemoteConfig.java
@@ -5,6 +5,7 @@ import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardCredentials;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.Util;
@@ -82,6 +83,7 @@ public class UserRemoteConfig extends AbstractDescribableImpl<UserRemoteConfig> 
     @Extension
     public static class DescriptorImpl extends Descriptor<UserRemoteConfig> {
 
+        @SuppressFBWarnings(value="NP_NULL_PARAM_DEREF", justification="pending https://github.com/jenkinsci/credentials-plugin/pull/68")
         public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Item project,
                                                      @QueryParameter String url,
                                                      @QueryParameter String credentialsId) {
