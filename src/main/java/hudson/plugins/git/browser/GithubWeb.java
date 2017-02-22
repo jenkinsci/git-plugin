7fb16cbe8ff5dbdb611afdd68b36d26237f89204
Mark Waite <mark.earl.waite@gmail.com>
2017-02-21 22:12:21 -0700

    Remove unused GithubWeb imports

--- src/main/java/hudson/plugins/git/browser/GithubWeb.java
+++ src/main/java/hudson/plugins/git/browser/GithubWeb.java
@@ -1,28 +1,18 @@
 package hudson.plugins.git.browser;
 
-import hudson.EnvVars;
 import hudson.Extension;
-import hudson.model.AbstractProject;
 import hudson.model.Descriptor;
-import hudson.model.EnvironmentContributor;
-import hudson.model.ItemGroup;
-import hudson.model.Job;
-import hudson.model.TaskListener;
 import hudson.plugins.git.GitChangeSet;
 import hudson.plugins.git.GitChangeSet.Path;
 import hudson.scm.EditType;
 import hudson.scm.RepositoryBrowser;
 import net.sf.json.JSONObject;
 
-import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * Git Browser URLs
