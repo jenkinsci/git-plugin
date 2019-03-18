1a75703153b8c6def57b1be3e5d22c8a5640f3e2
Mark Waite <mark.earl.waite@gmail.com>
2019-03-17 18:01:45 -0600

    Add another ancient git exclusion

--- src/test/java/hudson/plugins/git/GitChangeSetPluginHistoryTest.java
+++ src/test/java/hudson/plugins/git/GitChangeSetPluginHistoryTest.java
@@ -61,6 +61,7 @@ public class GitChangeSetPluginHistoryTest {
         "a571899e",
         "b9e497b0",
         "bc71cd2d",
+        "bca98ea9",
         "c73b4ff3",
         "dcd329f4",
         "edf066f3",
