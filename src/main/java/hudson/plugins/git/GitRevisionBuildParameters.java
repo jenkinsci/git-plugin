e969982bc1032ae57aeee6be7356d6fd171cfb15
Mark Waite <mark.earl.waite@gmail.com>
2019-10-16 14:07:34 -0600

    Adapt to promoted-builds plugin 3.4 API replacement
    Promotion.getTarget() is now Promotion.getTargetBuild().

--- src/main/java/hudson/plugins/git/GitRevisionBuildParameters.java
+++ src/main/java/hudson/plugins/git/GitRevisionBuildParameters.java
@@ -59,7 +59,7 @@ public class GitRevisionBuildParameters extends AbstractBuildParameters {
 		if (data == null && Jenkins.getInstance().getPlugin("promoted-builds") != null) {
             if (build instanceof hudson.plugins.promoted_builds.Promotion) {
                 // We are running as a build promotion, so have to retrieve the git scm from target job
-                data = ((hudson.plugins.promoted_builds.Promotion) build).getTarget().getAction(BuildData.class);
+                data = ((hudson.plugins.promoted_builds.Promotion) build).getTargetBuild().getAction(BuildData.class);
             }
         }
         if (data == null) {
