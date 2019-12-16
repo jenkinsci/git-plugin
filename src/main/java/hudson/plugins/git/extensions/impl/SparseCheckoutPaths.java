e7a74a3ef27535a61abfb0ac768eb66b66b0e379
René Scheibe <rene.scheibe@gmail.com>
2019-12-16 22:03:37 +0100

    Remove unused assignments
    These values are never used as the variables are always overwritten.

--- src/main/java/hudson/plugins/git/extensions/impl/SparseCheckoutPaths.java
+++ src/main/java/hudson/plugins/git/extensions/impl/SparseCheckoutPaths.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class SparseCheckoutPaths extends GitSCMExtension {
-    private List<SparseCheckoutPath> sparseCheckoutPaths = Collections.emptyList();
+    private List<SparseCheckoutPath> sparseCheckoutPaths;
 
     @DataBoundConstructor
     public SparseCheckoutPaths(List<SparseCheckoutPath> sparseCheckoutPaths) {
