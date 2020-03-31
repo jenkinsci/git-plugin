7203528aceb353ef22fd0f3173e6d9646bcfb2e5
Mark Waite <mark.earl.waite@gmail.com>
2020-03-30 21:51:25 -0600

    Remove mocks from crumb exclusion test
    Since the test already had a JenkinsRule, use it to make passing
    and failing HTTP requests to the notifyCommit URL and to other
    URL's.
    
    Uses a single JenkinsRule for the entire class of tests, since
    none of them alter the state of Jenkins enough to justify
    restarting Jenkins.  Saves the time of stopping and starting
    Jenkins for each test.

--- src/test/java/hudson/plugins/git/GitStatusCrumbExclusionTest.java
+++ src/test/java/hudson/plugins/git/GitStatusCrumbExclusionTest.java
@@ -1,79 +1,297 @@
 package hudson.plugins.git;
 
-import hudson.security.csrf.CrumbFilter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
+import org.junit.ClassRule;
 import org.junit.Test;
-
-import javax.servlet.FilterChain;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.util.Collections;
-
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.jvnet.hudson.test.JenkinsRule;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Check that no crumb is required for successful calls to notifyCommit.
+ */
 public class GitStatusCrumbExclusionTest {
 
-    @Rule
-    public JenkinsRule r = new JenkinsRule();
+    @ClassRule
+    public static JenkinsRule r = new JenkinsRule();
 
-    private CrumbFilter filter;
-    private HttpServletRequest req;
-    private HttpServletResponse resp;
-    private FilterChain chain;
+    private static final String GIT_REPO_URL = "https://github.com/jenkinsci/git-client-plugin";
 
-    private static String systemPropertyPreviousValue;
+    private final URL notifyCommitURL;
+    private final URL manageURL; // Valid URL that is not notifyCommit
+    private final URL invalidURL; // Jenkins internal URL that reports page not found
 
-    @BeforeClass
-    public static void setProps() {
-        systemPropertyPreviousValue = System.getProperty("hudson.security.csrf.CrumbFilter.UNPROCESSED_PATHINFO");
-        System.setProperty("hudson.security.csrf.CrumbFilter.UNPROCESSED_PATHINFO", "true");
-    }
+    private final String urlArgument;
+    private final byte[] urlArgumentBytes;
 
-    @AfterClass
-    public static void unsetProps() {
-        if (systemPropertyPreviousValue == null) {
-            System.clearProperty("hudson.security.csrf.CrumbFilter.UNPROCESSED_PATHINFO");
-        } else {
-            System.setProperty("hudson.security.csrf.CrumbFilter.UNPROCESSED_PATHINFO", systemPropertyPreviousValue);
+    private final String separator;
+    private final byte[] separatorBytes;
+
+    private final String branchArgument;
+    private final byte[] branchArgumentBytes;
+
+    public GitStatusCrumbExclusionTest() throws IOException {
+        String jenkinsUrl = r.getURL().toExternalForm();
+        if (!jenkinsUrl.endsWith("/")) {
+            jenkinsUrl = jenkinsUrl + "/";
         }
+        notifyCommitURL = new URL(jenkinsUrl + "git/notifyCommit");
+        manageURL = new URL(jenkinsUrl + "manage");
+        invalidURL = new URL(jenkinsUrl + "jobs/no-such-job");
+
+        urlArgument = "url=" + GIT_REPO_URL;
+        urlArgumentBytes = urlArgument.getBytes(StandardCharsets.UTF_8);
+
+        separator = "&";
+        separatorBytes = separator.getBytes(StandardCharsets.UTF_8);
+
+        branchArgument = "branches=origin/some-branch-name";
+        branchArgumentBytes = branchArgument.getBytes(StandardCharsets.UTF_8);
     }
+
+    private HttpURLConnection connectionPOST;
 
     @Before
-    public void before() {
-        filter = new CrumbFilter();
-        req = mock(HttpServletRequest.class);
-        resp = mock(HttpServletResponse.class);
-        chain = mock(FilterChain.class);
+    public void connectWithPOST() throws IOException {
+        URL postURL = notifyCommitURL;
+        connectionPOST = (HttpURLConnection) postURL.openConnection();
+        connectionPOST.setRequestMethod("POST");
+        connectionPOST.setDoOutput(true);
+    }
+
+    @After
+    public void disconnectFromPOST() {
+        connectionPOST.disconnect();
+    }
+
+    /*
+     * POST tests.
+     */
+    @Test
+    public void testPOSTValidPathNoArgument() throws Exception {
+        assertThat(connectionPOST.getResponseCode(), is(HttpURLConnection.HTTP_INTERNAL_ERROR));
+        assertThat(connectionPOST.getResponseMessage(), is("Server Error"));
     }
 
     @Test
-    public void testNotifyCommit() throws Exception {
-        when(req.getPathInfo()).thenReturn("/git/notifyCommit");
-        when(req.getMethod()).thenReturn("POST");
-        when(req.getParameterNames()).thenReturn(Collections.<String>emptyEnumeration());
-        filter.doFilter(req, resp, chain);
-        verify(resp, never()).sendError(anyInt(), anyString());
+    public void testPOSTValidPathMandatoryArgument() throws Exception {
+        try (OutputStream os = connectionPOST.getOutputStream()) {
+            os.write(urlArgumentBytes);
+        }
+        assertThat(connectionPOST.getResponseCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(connectionPOST.getResponseMessage(), is("OK"));
     }
 
     @Test
-    public void testInvalidPath() throws Exception {
-        when(req.getPathInfo()).thenReturn("/git/somethingElse");
-        when(req.getMethod()).thenReturn("POST");
-        when(req.getParameterNames()).thenReturn(Collections.<String>emptyEnumeration());
-        filter.doFilter(req, resp, chain);
-        verify(resp, times(1)).sendError(anyInt(), anyString());
+    public void testPOSTValidPathEmptyMandatoryArgument() throws Exception {
+        try (OutputStream os = connectionPOST.getOutputStream()) {
+            String urlEmptyArgument = "url="; // Empty argument is not a valid URL
+            byte[] urlEmptyArgumentBytes = urlEmptyArgument.getBytes(StandardCharsets.UTF_8);
+            os.write(urlEmptyArgumentBytes);
+        }
+        assertThat(connectionPOST.getResponseCode(), is(HttpURLConnection.HTTP_BAD_REQUEST));
+        assertThat(connectionPOST.getResponseMessage(), is("Bad Request"));
+    }
+
+    @Test
+    public void testPOSTValidPathBadURLInMandatoryArgument() throws Exception {
+        try (OutputStream os = connectionPOST.getOutputStream()) {
+            String urlBadArgument = "url=" + "http://256.256.256.256/"; // Not a valid URI per Java 8 javadoc
+            byte[] urlBadArgumentBytes = urlBadArgument.getBytes(StandardCharsets.UTF_8);
+            os.write(urlBadArgumentBytes);
+        }
+        assertThat(connectionPOST.getResponseCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(connectionPOST.getResponseMessage(), is("OK"));
+    }
+
+    @Test
+    public void testPOSTValidPathMandatoryAndOptionalArgument() throws Exception {
+        try (OutputStream os = connectionPOST.getOutputStream()) {
+            os.write(urlArgumentBytes);
+            os.write(separatorBytes);
+            os.write(branchArgumentBytes);
+        }
+        assertThat(connectionPOST.getResponseCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(connectionPOST.getResponseMessage(), is("OK"));
+    }
+
+    @Test
+    public void testPOSTValidPathOnlyOptionalArgument() throws Exception {
+        try (OutputStream os = connectionPOST.getOutputStream()) {
+            os.write(branchArgumentBytes);
+        }
+        assertThat(connectionPOST.getResponseCode(), is(HttpURLConnection.HTTP_INTERNAL_ERROR));
+        assertThat(connectionPOST.getResponseMessage(), is("Server Error"));
+    }
+
+    @Test
+    public void testPOSTInvalidPathNoArgument() throws Exception {
+        URL invalidPathURL = invalidURL;
+        HttpURLConnection invalidPathConnection = (HttpURLConnection) invalidPathURL.openConnection();
+        invalidPathConnection.setRequestMethod("POST");
+        invalidPathConnection.setDoOutput(true);
+        assertThat(invalidPathConnection.getResponseCode(), is(HttpURLConnection.HTTP_FORBIDDEN));
+        assertThat(invalidPathConnection.getResponseMessage(), is("Forbidden"));
+        invalidPathConnection.disconnect();
+    }
+
+    @Test
+    public void testPOSTInvalidPathMandatoryArgument() throws Exception {
+        URL invalidPathURL = invalidURL;
+        HttpURLConnection invalidPathConnection = (HttpURLConnection) invalidPathURL.openConnection();
+        invalidPathConnection.setRequestMethod("POST");
+        invalidPathConnection.setDoOutput(true);
+        try (OutputStream os = invalidPathConnection.getOutputStream()) {
+            os.write(urlArgumentBytes);
+        }
+        assertThat(invalidPathConnection.getResponseCode(), is(HttpURLConnection.HTTP_FORBIDDEN));
+        assertThat(invalidPathConnection.getResponseMessage(), is("Forbidden"));
+        invalidPathConnection.disconnect();
+    }
+
+    @Test
+    public void testPOSTInvalidPathMandatoryAndOptionalArgument() throws Exception {
+        URL invalidPathURL = invalidURL;
+        HttpURLConnection invalidPathConnection = (HttpURLConnection) invalidPathURL.openConnection();
+        invalidPathConnection.setRequestMethod("POST");
+        invalidPathConnection.setDoOutput(true);
+        try (OutputStream os = invalidPathConnection.getOutputStream()) {
+            os.write(urlArgumentBytes);
+            os.write(separatorBytes);
+            os.write(branchArgumentBytes);
+        }
+        assertThat(invalidPathConnection.getResponseCode(), is(HttpURLConnection.HTTP_FORBIDDEN));
+        assertThat(invalidPathConnection.getResponseMessage(), is("Forbidden"));
+        invalidPathConnection.disconnect();
+    }
+
+    @Test
+    public void testPOSTManageNoArgument() throws Exception {
+        URL postURL = manageURL;
+        HttpURLConnection manageConnection = (HttpURLConnection) postURL.openConnection();
+        manageConnection.setRequestMethod("POST");
+        manageConnection.setDoOutput(true);
+        assertThat(manageConnection.getResponseCode(), is(HttpURLConnection.HTTP_FORBIDDEN));
+        assertThat(manageConnection.getResponseMessage(), is("Forbidden"));
+        manageConnection.disconnect();
+    }
+
+    @Test
+    public void testPOSTManageMandatoryArgument() throws Exception {
+        URL postURL = manageURL;
+        HttpURLConnection manageConnection = (HttpURLConnection) postURL.openConnection();
+        manageConnection.setRequestMethod("POST");
+        manageConnection.setDoOutput(true);
+        try (OutputStream os = manageConnection.getOutputStream()) {
+            os.write(urlArgumentBytes);
+        }
+        assertThat(manageConnection.getResponseCode(), is(HttpURLConnection.HTTP_FORBIDDEN));
+        assertThat(manageConnection.getResponseMessage(), is("Forbidden"));
+        manageConnection.disconnect();
+    }
+
+    @Test
+    public void testPOSTManageMandatoryAndOptionalArgument() throws Exception {
+        URL postURL = manageURL;
+        HttpURLConnection manageConnection = (HttpURLConnection) postURL.openConnection();
+        manageConnection.setRequestMethod("POST");
+        manageConnection.setDoOutput(true);
+        try (OutputStream os = manageConnection.getOutputStream()) {
+            os.write(urlArgumentBytes);
+            os.write(separatorBytes);
+            os.write(branchArgumentBytes);
+        }
+        assertThat(manageConnection.getResponseCode(), is(HttpURLConnection.HTTP_FORBIDDEN));
+        assertThat(manageConnection.getResponseMessage(), is("Forbidden"));
+        manageConnection.disconnect();
+    }
+
+    /*
+     * GET tests.
+     */
+    @Test
+    public void testGETValidPathNoArgument() throws Exception {
+        URL getURL = notifyCommitURL;
+        HttpURLConnection connectionGET = (HttpURLConnection) getURL.openConnection();
+        connectionGET.setRequestMethod("GET");
+        connectionGET.connect();
+        assertThat(connectionGET.getResponseCode(), is(HttpURLConnection.HTTP_INTERNAL_ERROR));
+        assertThat(connectionGET.getResponseMessage(), is("Server Error"));
+        connectionGET.disconnect();
+    }
+
+    @Test
+    public void testGETValidPathMandatoryArgument() throws Exception {
+        URL getURL = new URL(notifyCommitURL + "?" + urlArgument);
+        HttpURLConnection connectionGET = (HttpURLConnection) getURL.openConnection();
+        connectionGET.setRequestMethod("GET");
+        connectionGET.connect();
+        assertThat(connectionGET.getResponseCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(connectionGET.getResponseMessage(), is("OK"));
+        connectionGET.disconnect();
+    }
+
+    @Test
+    public void testGETValidPathMandatoryAndOptionalArgument() throws Exception {
+        URL getURL = new URL(notifyCommitURL + "?" + urlArgument + separator + branchArgument);
+        HttpURLConnection connectionGET = (HttpURLConnection) getURL.openConnection();
+        connectionGET.setRequestMethod("GET");
+        connectionGET.connect();
+        assertThat(connectionGET.getResponseCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(connectionGET.getResponseMessage(), is("OK"));
+        connectionGET.disconnect();
+    }
+
+    @Test
+    public void testGETInvalidPath() throws Exception {
+        URL getURL = invalidURL;
+        HttpURLConnection connectionGET = (HttpURLConnection) getURL.openConnection();
+        connectionGET.setRequestMethod("GET");
+        connectionGET.connect();
+        assertThat(connectionGET.getResponseCode(), is(HttpURLConnection.HTTP_NOT_FOUND));
+        assertThat(connectionGET.getResponseMessage(), is("Not Found"));
+        connectionGET.disconnect();
+    }
+
+    @Test
+    public void testGETInvalidPathWithArgument() throws Exception {
+        URL getURL = new URL(invalidURL + "?" + urlArgument);
+        HttpURLConnection connectionGET = (HttpURLConnection) getURL.openConnection();
+        connectionGET.setRequestMethod("GET");
+        connectionGET.connect();
+        assertThat(connectionGET.getResponseCode(), is(HttpURLConnection.HTTP_NOT_FOUND));
+        assertThat(connectionGET.getResponseMessage(), is("Not Found"));
+        connectionGET.disconnect();
+    }
+
+    @Test
+    public void testGETManagePath() throws Exception {
+        URL getURL = manageURL;
+        HttpURLConnection connectionGET = (HttpURLConnection) getURL.openConnection();
+        connectionGET.setRequestMethod("GET");
+        connectionGET.connect();
+        assertThat(connectionGET.getResponseCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(connectionGET.getResponseMessage(), is("OK"));
+        connectionGET.disconnect();
+    }
+
+    @Test
+    public void testGETManagePathWithArgument() throws Exception {
+        URL getURL = new URL(manageURL + "?" + urlArgument);
+        HttpURLConnection connection = (HttpURLConnection) getURL.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        assertThat(connection.getResponseCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(connection.getResponseMessage(), is("OK"));
+        connection.disconnect();
     }
 }
