06dbaf92786058eb55bc4a13103e30992aad3082
Mark Waite <mark.earl.waite@gmail.com>
2020-04-11 17:45:05 -0600

    Remove no-op deprecated call to noCheckout()
    Javadoc and all implementations confirmed as no-op.

--- src/main/java/hudson/plugins/git/extensions/impl/SparseCheckoutPaths.java
+++ src/main/java/hudson/plugins/git/extensions/impl/SparseCheckoutPaths.java
@@ -36,7 +36,6 @@ public class SparseCheckoutPaths extends GitSCMExtension {
     public void decorateCloneCommand(GitSCM scm, Run<?, ?> build, GitClient git, TaskListener listener, CloneCommand cmd) throws IOException, InterruptedException, GitException {
         if (! sparseCheckoutPaths.isEmpty()) {
             listener.getLogger().println("Using no checkout clone with sparse checkout.");
-            cmd.noCheckout();
         }
     }
 
