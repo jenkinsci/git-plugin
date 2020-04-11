59c156e1c9786be70d62e74631e4ba62c7ffc61e
Mark Waite <mark.earl.waite@gmail.com>
2020-04-11 17:49:04 -0600

    Add SparseCheckoutPaths tests

---
+++ src/test/java/hudson/plugins/git/extensions/impl/LogHandler.java
@@ -0,0 +1,58 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Mark Waite.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package hudson.plugins.git.extensions.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * Recording log handler to allow assertions on logging. Not intended for use
+ * outside this package. Not intended for use outside tests.
+ *
+ * @author <a href="mailto:mark.earl.waite@gmail.com">Mark Waite</a>
+ */
+public class LogHandler extends Handler {
+
+    private List<String> messages = new ArrayList<>();
+
+    @Override
+    public void publish(LogRecord lr) {
+        messages.add(lr.getMessage());
+    }
+
+    @Override
+    public void flush() {
+    }
+
+    @Override
+    public void close() throws SecurityException {
+        messages = new ArrayList<>();
+    }
+
+    /* package */ List<String> getMessages() {
+        return messages;
+    }
+}
