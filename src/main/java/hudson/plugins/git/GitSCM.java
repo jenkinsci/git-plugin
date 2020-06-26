236b064fdf4353a1feaebe1d8676a5a95e899a9d
Carsten Pfeiffer <carsten.pfeiffer@gebit.de>
2020-06-26 17:12:51 +0200

    Clarify the consequences of setting user.name and user.email
    It only sets some environment variables and does *not* alter
    and (local or global) git configuration.

--- src/main/java/hudson/plugins/git/GitSCM.java
+++ src/main/java/hudson/plugins/git/GitSCM.java
@@ -1563,7 +1563,7 @@ public class GitSCM extends GitSCMBackwardCompatibility {
         }
 
         /**
-         * Global setting to be used in call to "git config user.name".
+         * Global setting to be used to set GIT_COMMITTER_NAME and GIT_AUTHOR_NAME.
          * @return user.name value
          */
         public String getGlobalConfigName() {
@@ -1571,7 +1571,7 @@ public class GitSCM extends GitSCMBackwardCompatibility {
         }
 
         /**
-         * Global setting to be used in call to "git config user.name".
+         * Global setting to be used to set GIT_COMMITTER_NAME and GIT_AUTHOR_NAME.
          * @param globalConfigName user.name value to be assigned
          */
         public void setGlobalConfigName(String globalConfigName) {
@@ -1579,7 +1579,7 @@ public class GitSCM extends GitSCMBackwardCompatibility {
         }
 
         /**
-         * Global setting to be used in call to "git config user.email".
+         * Global setting to be used to set GIT_COMMITTER_EMAIL and GIT_AUTHOR_EMAIL.
          * @return user.email value
          */
         public String getGlobalConfigEmail() {
@@ -1587,7 +1587,7 @@ public class GitSCM extends GitSCMBackwardCompatibility {
         }
 
         /**
-         * Global setting to be used in call to "git config user.email".
+         * Global setting to be used to set GIT_COMMITTER_EMAIL and GIT_AUTHOR_EMAIL.
          * @param globalConfigEmail user.email value to be assigned
          */
         public void setGlobalConfigEmail(String globalConfigEmail) {
