f1721a6e15206012a6e4c5321c201b37a565fbcf
Rishabh Budhouliya <rishabhbudhouliya@gmail.com>
2020-08-05 10:07:01 +0530

    Add support to determine if JGit can be used or not By implementing this method, this extension is conveying that in the case a user wants to use timeout, JGit will not be able to perform the git client operations as it is not yet supported.

--- src/main/java/hudson/plugins/git/extensions/impl/CheckoutOption.java
+++ src/main/java/hudson/plugins/git/extensions/impl/CheckoutOption.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.Objects;
 import org.jenkinsci.plugins.gitclient.CheckoutCommand;
 import org.jenkinsci.plugins.gitclient.GitClient;
+import org.jenkinsci.plugins.gitclient.UnsupportedCommand;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -47,6 +48,11 @@ public class CheckoutOption extends FakeGitSCMExtension {
      */
     @Override
     public void decorateCheckoutCommand(GitSCM scm, Run<?, ?> build, GitClient git, TaskListener listener, CheckoutCommand cmd) throws IOException, InterruptedException, GitException {
+        cmd.timeout(timeout);
+    }
+
+    @Override
+    public void determineSupportForJGit(GitSCM scm, UnsupportedCommand cmd) {
         cmd.timeout(timeout);
     }
 
