37e67e5fe669d4cfc1d168ab1eaafc96fd302eca
Mark Waite <mark.earl.waite@gmail.com>
2020-12-10 19:23:15 -0700

    Revise test to check system vars and Jenkins vars
    Checks both system environment variables (like USER on Unix variants
    and USERNAME on Windows) and Jenkins provided environment variables
    (like JOB_NAME).
    
    Asserts that the variable name does not appear in the build log.
    
    Uses a branch based on the value of the variable and asserts that the
    job is successful.  A successful job means that the branch name based
    on the value of the variable was found and used in the job.
    
    Does not assert that the specific commit from the branch is the one that
    is used in the job.  That could be added.

--- src/test/java/hudson/plugins/git/extensions/impl/CloneOptionHonorRefSpecTest.java
+++ src/test/java/hudson/plugins/git/extensions/impl/CloneOptionHonorRefSpecTest.java
@@ -3,73 +3,118 @@ package hudson.plugins.git.extensions.impl;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
-import org.jvnet.hudson.test.Issue;
 import hudson.plugins.git.AbstractGitTestCase;
 import hudson.plugins.git.BranchSpec;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.UserRemoteConfig;
-import org.eclipse.jgit.transport.RefSpec;
-import org.jenkinsci.plugins.gitclient.CloneCommand;
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.jvnet.hudson.test.Issue;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(Parameterized.class)
 public class CloneOptionHonorRefSpecTest extends AbstractGitTestCase {
 
-    private String refSpecVar;
+    private final String refSpecName;
+    private final String refSpecExpectedValue;
+    private final Boolean honorRefSpec;
 
-    public CloneOptionHonorRefSpecTest(String useRefSpecVariable) {
-        this.refSpecVar = useRefSpecVariable;
+    private static final Random random = new Random();
+
+    public CloneOptionHonorRefSpecTest(String refSpecName, String refSpecExpectedValue, Boolean honorRefSpec) {
+        this.refSpecName = refSpecName;
+        this.refSpecExpectedValue = refSpecExpectedValue;
+        this.honorRefSpec = honorRefSpec;
     }
 
-    @Parameterized.Parameters(name = "{0}")
+    private static String getExpectedValue(String reference) {
+        if (reference.startsWith("JOB_")) {
+            return "test0";
+        }
+        if (reference.contains("USER")) {
+            return System.getProperty("user.name", "java-user.name-property-not-found");
+        }
+        return "not-master"; // fake value for other variables
+    }
+
+    @Parameterized.Parameters(name = "{0}-{1}-{2}")
     public static Collection permuteRefSpecVariable() {
         List<Object[]> values = new ArrayList<>();
-        String[] allowed = {"${GIT_REVISION}", "${GIT_COMMIT}"};
-        for (String refSpecValue : allowed) {
-            Object[] combination = {refSpecValue};
+        /* Should behave same with honor refspec enabled or disabled */
+        boolean honorRefSpec = random.nextBoolean();
+
+        /* Variables set by Jenkins */
+        String[] allowed = {"JOB_BASE_NAME", "JOB_NAME", "JENKINS_USERNAME"};
+        for (String refSpecName : allowed) {
+            String refSpecExpectedValue = getExpectedValue(refSpecName);
+            Object[] combination = {refSpecName, refSpecExpectedValue, honorRefSpec};
             values.add(combination);
+            honorRefSpec = !honorRefSpec;
         }
+
+        /* Variable set by the operating system */
+        String refSpecName = isWindows() ? "USERNAME" : "USER";
+        String refSpecExpectedValue = getExpectedValue(refSpecName);
+        Object[] combination = {refSpecName, refSpecExpectedValue, honorRefSpec};
+        values.add(combination);
+
         return values;
     }
 
     /**
-     * This test confirms behavior of refspecs on initial clone with expanded variables.
+     * This test confirms behavior of refspecs on initial clone with expanded
+     * variables. When an environment variable reference is embedded in the
+     * refspec, it should be expanded in all cases.
+     *
      * @throws Exception on error
      */
     @Test
     @Issue("JENKINS-56063")
     public void testRefSpecWithExpandedVariables() throws Exception {
-        List<UserRemoteConfig> repos = new ArrayList<>();
-        repos.add(new UserRemoteConfig(testRepo.gitDir.getAbsolutePath(), "origin", "+refs/heads/master:refs/remotes/origin/master" + refSpecVar, null));
-
-        /* Set CloneOption to honor refspec on initial clone with expanded var */
-        FreeStyleProject projectWithMasterExpanded = setupProject(repos, Collections.singletonList(new BranchSpec("master" + refSpecVar)), null, false, null);
-        CloneOption cloneOptionMasterExpanded = new CloneOption(false, null, null);
-        cloneOptionMasterExpanded.setHonorRefspec(true);
-        ((GitSCM)projectWithMasterExpanded.getScm()).getExtensions().add(cloneOptionMasterExpanded);
-
-        /* Set CloneOption to honor refspec on initial clone without expanded var, should fail */
-        FreeStyleProject projectWithMaster = setupProject(repos, Collections.singletonList(new BranchSpec("master")), null, false, null);
-        CloneOption cloneOptionMaster = new CloneOption(false, null, null);
-        cloneOptionMaster.setHonorRefspec(true);
-        ((GitSCM)projectWithMaster.getScm()).getExtensions().add(cloneOptionMaster);
 
         // create initial commit
         final String commitFile1 = "commitFile1";
-        commit(commitFile1, johnDoe, "Commit in master");
-        // create branch and make initial commit
-        git.checkout().ref("master").branch("foo").execute();
-        commit(commitFile1, johnDoe, "Commit in foo");
+        commit(commitFile1, johnDoe, "Commit in master branch");
 
-        build(projectWithMaster, Result.FAILURE);
-        build(projectWithMasterExpanded, Result.SUCCESS, commitFile1);
+        // create branch and make initial commit
+        git.checkout().ref("master").branch(refSpecExpectedValue).execute();
+        commit(commitFile1, johnDoe, "Commit in '" + refSpecExpectedValue + "' branch");
+
+        // Use the variable reference in the refspec
+        // Should be expanded by the clone option whether or not honor refspec is enabled
+        List<UserRemoteConfig> repos = new ArrayList<>();
+        repos.add(new UserRemoteConfig(
+                testRepo.gitDir.getAbsolutePath(),
+                "origin",
+                "+refs/heads/${" + refSpecName + "}:refs/remotes/origin/${" + refSpecName + "}", null));
+
+        /* Use the variable or its value as the branch name.
+         * Same result expected in either case.
+         */
+        String branchName = random.nextBoolean() ? "${" + refSpecName + "}" : refSpecExpectedValue;
+        FreeStyleProject project = setupProject(repos, Collections.singletonList(new BranchSpec(branchName)), null, false, null);
+
+        /* Same result expected whether refspec honored or not */
+        CloneOption cloneOption = new CloneOption(false, null, null);
+        cloneOption.setHonorRefspec(honorRefSpec);
+        ((GitSCM) project.getScm()).getExtensions().add(cloneOption);
+
+        FreeStyleBuild b = build(project, Result.SUCCESS, commitFile1);
+        /* Check that unexpanded refspec name is not in the log */
+        List<String> buildLog = b.getLog(50);
+        assertThat(buildLog, not(hasItem(containsString("${" + refSpecName + "}"))));
+    }
+
+    private static boolean isWindows() {
+        return false;
     }
 }
