ba2bb450dd08885635de4da97c2bc84e668f46c2
Mark Waite <mark.earl.waite@gmail.com>
2021-01-09 06:20:13 -0700

    Improve BuildTest clarity and coverage

--- src/test/java/hudson/plugins/git/util/BuildTest.java
+++ src/test/java/hudson/plugins/git/util/BuildTest.java
@@ -14,10 +14,19 @@ public class BuildTest {
 
     private final int BUILD_NUMBER = 1;
     private final Result BUILD_RESULT = Result.NOT_BUILT;
+
+    /* A build with no revision */
+    private final Build nullRevisionBuild = new Build(null, BUILD_NUMBER, BUILD_RESULT);
+
+    /* A build of revision that has marked the same revision */
     private final ObjectId objectId = ObjectId.fromString("b04752abaaa9ee3112b0c9d1910093c977d4e583");
     private final Revision revision = new Revision(objectId);
     private final Build build = new Build(revision, BUILD_NUMBER, BUILD_RESULT);
-    private final Build nullRevisionBuild = new Build(null, BUILD_NUMBER, BUILD_RESULT);
+
+    /* A build of revision that has marked a different revision */
+    private final ObjectId markedId = ObjectId.fromString("deedbeadfeedcededeafc9d1910093c977d4e583");
+    private final Revision markedRevision = new Revision(markedId);
+    private final Build markedBuild = new Build(markedRevision, revision, BUILD_NUMBER, BUILD_RESULT);
 
     @Test
     public void testGetSHA1() {
@@ -36,11 +45,7 @@ public class BuildTest {
 
     @Test
     public void testGetMarkedDifferentRevision() {
-        ObjectId differentId = ObjectId.fromString("deedbeadfeedcededeafc9d1910093c977d4e583");
-        Revision markedRevision = new Revision(differentId);
-        Build differentBuild = new Build(markedRevision, revision, BUILD_NUMBER, BUILD_RESULT);
-        assertThat(differentBuild.getMarked(), is(markedRevision));
-        assertThat(build.getMarked(), is(revision));
+        assertThat(markedBuild.getMarked(), is(markedRevision));
     }
 
     @Test
@@ -73,6 +78,12 @@ public class BuildTest {
     @Test
     public void testIsFor() {
         assertThat(build.isFor(revision.getSha1String()), is(true));
+    }
+
+    @Test
+    public void testIsForMarkedRevision() {
+        assertThat(markedBuild.isFor(revision.getSha1String()), is(true));
+        assertThat(markedBuild.isFor(markedRevision.getSha1String()), is(false));
     }
 
     @Test
