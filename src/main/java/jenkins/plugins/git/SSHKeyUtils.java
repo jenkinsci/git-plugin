2bc31408aad7d88ee407247cceda97de6f14b5b8
Harshit Chopra <harshitchopra0712@gmail.com>
2021-07-21 23:48:47 +0530

    Contract to support Git SSH binding implementation

---
+++ src/main/java/jenkins/plugins/git/SSHKeyUtils.java
@@ -0,0 +1,54 @@
+package jenkins.plugins.git;
+
+import com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey;
+import hudson.FilePath;
+import hudson.util.Secret;
+import jenkins.bouncycastle.api.PEMEncodable;
+import org.jenkinsci.plugins.gitclient.GitClient;
+
+import java.io.IOException;
+import java.security.UnrecoverableKeyException;
+
+public interface SSHKeyUtils {
+
+    static String getPrivateKey(SSHUserPrivateKey credentials) {
+        return credentials.getPrivateKeys().get(0);
+    }
+
+    static String getPassphrase(SSHUserPrivateKey credentials) {
+        return Secret.toString(credentials.getPassphrase());
+    }
+
+    static boolean isPrivateKeyEncrypted(String passphrase) {
+        return passphrase.isEmpty() ? false : true;
+    }
+
+    //TODO Use getSSHExecutable method
+    default String getSSHExePathInWin(GitClient git) throws IOException, InterruptedException {
+//      return ((CliGitAPIImpl) git).getSSHExecutable());
+        return "ssh";
+    }
+
+    default FilePath getPrivateKeyFile(SSHUserPrivateKey credentials, FilePath workspace) throws InterruptedException, IOException {
+        FilePath tempKeyFile = workspace.createTempFile("private", ".key");
+        final String privateKeyValue = getPassphrase(credentials);
+        final String passphraseValue = getPassphrase(credentials);
+        try {
+            if (isPrivateKeyEncrypted(privateKeyValue)) {
+                if (OpenSSHKeyFormatImpl.isOpenSSHFormat(privateKeyValue)) {
+                    OpenSSHKeyFormatImpl openSSHKeyFormat = new OpenSSHKeyFormatImpl(privateKeyValue, passphraseValue);
+                    tempKeyFile.write(openSSHKeyFormat.getDecodedPrivateKey(), null);
+                } else {
+                    tempKeyFile.write(PEMEncodable.decode(privateKeyValue, passphraseValue.toCharArray()).encode(), null);
+                }
+            } else {
+                tempKeyFile.write(privateKeyValue, null);
+            }
+            tempKeyFile.chmod(0500);
+            return tempKeyFile;
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
