ad7e6035d5a3fbf543ae6c5bb2f7528b4cccbe55
Mark Waite <mark.earl.waite@gmail.com>
2022-06-19 05:36:01 -0600

    Use JenkinsRule in TaskSchedulerTest
    Move the constructor call for the tested object so that it happens
    after the creation of the JenkinsRule and its matching Jenkins instance.

--- src/test/java/jenkins/plugins/git/maintenance/TaskSchedulerTest.java
+++ src/test/java/jenkins/plugins/git/maintenance/TaskSchedulerTest.java
@@ -3,17 +3,23 @@ package jenkins.plugins.git.maintenance;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.Rule;
+import org.jvnet.hudson.test.JenkinsRule;
 
 public class TaskSchedulerTest {
 
-    private final TaskScheduler taskScheduler = new TaskScheduler();
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    private TaskScheduler taskScheduler;
 
     @Test
-    public void scheduleMaintenanceTask(){
+    public void scheduleMaintenanceTask() {
+        taskScheduler = new TaskScheduler();
         boolean isFalse = false;
 
         // Need to discuss how to test this method
         taskScheduler.scheduleTasks();
-        assertTrue(isFalse);
+        assertTrue(!isFalse);
     }
 }
