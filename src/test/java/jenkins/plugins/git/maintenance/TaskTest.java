0562df79ccd188495f6d352615a9960613cb0fca
Mark Waite <mark.earl.waite@gmail.com>
2022-06-19 08:34:05 -0600

    Add a few task tests
    Throw more general Exception from a test rather than ANTLRException
    to reduce future test maintenance.
    
    Add comment reminder that cron syntax would be a good candidate to
    parameterize the test in addition to the existing task type
    parameterization.

--- src/test/java/jenkins/plugins/git/maintenance/TaskTest.java
+++ src/test/java/jenkins/plugins/git/maintenance/TaskTest.java
@@ -1,6 +1,5 @@
 package jenkins.plugins.git.maintenance;
 
-import antlr.ANTLRException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -11,9 +10,10 @@ import java.util.Collection;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(Parameterized.class)
 public class TaskTest {
@@ -22,17 +22,17 @@ public class TaskTest {
     private final TaskType taskType;
     private final String taskName;
 
-    public TaskTest(Task task,TaskType taskType,String taskName){
+    public TaskTest(Task task, TaskType taskType, String taskName) {
         this.task = task;
         this.taskType = taskType;
         this.taskName = taskName;
     }
 
-    @Parameterized.Parameters(name="{0}")
-    public static Collection permuteTasks(){
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection permuteTasks() {
         List<Object[]> tasks = new ArrayList<>();
-        for(TaskType taskType : TaskType.values()){
-            Object[] task = {new Task(taskType),taskType,taskType.getTaskName()};
+        for (TaskType taskType : TaskType.values()) {
+            Object[] task = {new Task(taskType), taskType, taskType.getTaskName()};
             tasks.add(task);
         }
 
@@ -40,40 +40,104 @@ public class TaskTest {
     }
 
     @Test
-    public void getTaskType(){
-        assertEquals(taskType,task.getTaskType());
+    public void getTaskType() {
+        assertThat(task.getTaskType(), is(taskType));
     }
 
     @Test
-    public void getTaskName(){
-        assertEquals(taskName,task.getTaskName());
+    public void getTaskName() {
+        assertThat(task.getTaskName(), is(taskName));
     }
 
     @Test
-    public void setIsConfigured(){
-        boolean isConfigured = true;
-        task.setIsTaskConfigured(isConfigured);
-        assertEquals(isConfigured,task.getIsTaskConfigured());
+    public void setIsConfigured() {
+        task.setIsTaskConfigured(true);
+        assertTrue(task.getIsTaskConfigured());
     }
 
     @Test
-    public void checkCronSyntax() throws ANTLRException {
+    public void checkCronSyntax() throws Exception {
         String cronSyntax = "* * 1 * 1";
         task.setCronSyntax(cronSyntax);
-        assertEquals(cronSyntax,task.getCronSyntax());
+        assertThat(task.getCronSyntax(), is(cronSyntax));
     }
 
     @Test
-    public void isTaskExecutable() throws ANTLRException {
+    public void isTaskExecutable() throws Exception {
         Calendar cal = new GregorianCalendar();
         String cronSyntax = "* * * * *";
         task.setCronSyntax(cronSyntax);
         boolean isTaskExecutable = task.checkIsTaskExecutable(cal);
         assertTrue(isTaskExecutable);
+    }
 
-        cronSyntax = "H * * * *";
+    @Test
+    public void isTaskExecutableWithHSyntax() throws Exception {
+        Calendar cal = new GregorianCalendar();
+        String cronSyntax = "H * * * *";
         task.setCronSyntax(cronSyntax);
-        isTaskExecutable = task.checkIsTaskExecutable(cal);
+        boolean isTaskExecutable = task.checkIsTaskExecutable(cal);
         assertFalse(isTaskExecutable);
+    }
+
+    @Test
+    public void testGetTaskType() {
+        assertThat(task.getTaskType(), is(taskType));
+    }
+
+    @Test
+    public void testGetTaskName() {
+        assertThat(task.getTaskName(), is(taskName));
+    }
+
+    @Test
+    public void testGetCronSyntax() throws Exception {
+        String cronSyntax = "* * 1 * 1";
+        task.setCronSyntax(cronSyntax);
+        assertThat(task.getCronSyntax(), is(cronSyntax));
+    }
+
+    @Test
+    public void testSetIsTaskConfigured() {
+        task.setIsTaskConfigured(true);
+        assertTrue(task.getIsTaskConfigured());
+    }
+
+    @Test
+    public void testGetIsTaskConfigured() {
+        task.setIsTaskConfigured(true);
+        assertTrue(task.getIsTaskConfigured());
+    }
+
+    @Test
+    public void testGetIsTaskConfiguredFalse() {
+        task.setIsTaskConfigured(false);
+        assertFalse(task.getIsTaskConfigured());
+    }
+
+    // Syntax not yet supported by this class
+    // Should parameterize test for cron syntax as well
+    // @Test
+    public void testSetCronSyntaxDaily() throws Exception {
+        Calendar cal = new GregorianCalendar();
+        task.setCronSyntax("@daily");
+        assertTrue(task.checkIsTaskExecutable(cal));
+    }
+
+    // Should parameterize test for cron syntax as well
+    @Test
+    public void testCheckIsTaskExecutable() throws Exception {
+        Calendar cal = new GregorianCalendar();
+        String cronSyntax = "* * * * *";
+        task.setCronSyntax(cronSyntax);
+        assertTrue(task.checkIsTaskExecutable(cal));
+    }
+
+    // Should parameterize test for cron syntax as well
+    @Test
+    public void testCheckIsTaskExecutableWithHSyntax() throws Exception {
+        Calendar cal = new GregorianCalendar();
+        task.setCronSyntax("H * * * *");
+        assertFalse(task.checkIsTaskExecutable(cal));
     }
 }
