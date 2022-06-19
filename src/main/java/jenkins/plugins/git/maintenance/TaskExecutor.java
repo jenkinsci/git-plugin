2e0d8db16bf0ce8de8f26bfe28646a0dfc56e1dd
Mark Waite <mark.earl.waite@gmail.com>
2022-06-19 14:06:19 -0600

    Fix spotbugs warnings

--- src/main/java/jenkins/plugins/git/maintenance/TaskExecutor.java
+++ src/main/java/jenkins/plugins/git/maintenance/TaskExecutor.java
@@ -1,13 +1,21 @@
 package jenkins.plugins.git.maintenance;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskExecutor implements Runnable {
 
     List<Task> maintenanceQueue;
+
     public TaskExecutor(List<Task> maintenanceQueue){
-        this.maintenanceQueue = maintenanceQueue;
+        /* Defensive copy avoids risk of caller modifying the queue after object creation */
+        this.maintenanceQueue = new ArrayList<>(maintenanceQueue);
     }
+
+    public int queueLength() {
+        return maintenanceQueue.size();
+    }
+
     @Override
     public void run() {
         // Need a way to iterate through all the caches present on Jenkins controller.
