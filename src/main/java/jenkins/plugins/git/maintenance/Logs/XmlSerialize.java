249e1e4456e3f798853e09ff29c6df9dae71d9c6
hrushi20 <hrushikesh20thegreat@gmail.com>
2022-08-27 03:14:58 +0530

    Improve performance by reading data once from xml file

--- src/main/java/jenkins/plugins/git/maintenance/Logs/XmlSerialize.java
+++ src/main/java/jenkins/plugins/git/maintenance/Logs/XmlSerialize.java
@@ -18,33 +18,47 @@ public class XmlSerialize{
 
     File maintenanceRecordsFile;
 
+    RecordList recordList;
+
     public XmlSerialize(){
         this.xStream = new XStream(new DomDriver());
         // Need to change the Permission type. Todo need to read documentation and update security.
         this.xStream.addPermission(AnyTypePermission.ANY);
-        File rootDir = Jenkins.getInstanceOrNull().getRootDir();
-        this.maintenanceRecordsFile = new File(rootDir.getAbsolutePath(),"maintenanceRecords.xml");
+        Jenkins jenkins = Jenkins.getInstanceOrNull();
+
+        if(jenkins != null) {
+            File rootDir = jenkins.getRootDir();
+            this.maintenanceRecordsFile = new File(rootDir.getAbsolutePath(), "maintenanceRecords.xml");
+        }
     }
 
     RecordList fetchMaintenanceData(){
-        try {
-            RecordList recordList;
-            if (!maintenanceRecordsFile.exists()) {
-                recordList = new RecordList();
-            } else {
-                byte[] parsedXmlByteArr = Files.readAllBytes(Paths.get(maintenanceRecordsFile.getAbsolutePath()));
-                String parsedXmlString = new String(parsedXmlByteArr, StandardCharsets.UTF_8);
-
-                xStream.setClassLoader(RecordList.class.getClassLoader());
-                recordList = (RecordList) xStream.fromXML(parsedXmlString);
-            }
-
-            return recordList;
-        }catch (IOException e){
-           // Handle exception...
+        if(maintenanceRecordsFile == null){
+            // Need to log.....
+            return null;
         }
 
-        return null;
+        // Checks if recordList is loaded from xml. If not loaded, load it.
+        if(recordList == null) {
+
+            try {
+                RecordList recordList;
+                if (!maintenanceRecordsFile.exists()) {
+                    recordList = new RecordList();
+                } else {
+                    byte[] parsedXmlByteArr = Files.readAllBytes(Paths.get(maintenanceRecordsFile.getAbsolutePath()));
+                    String parsedXmlString = new String(parsedXmlByteArr, StandardCharsets.UTF_8);
+
+                    xStream.setClassLoader(RecordList.class.getClassLoader());
+                    recordList = (RecordList) xStream.fromXML(parsedXmlString);
+                }
+                this.recordList = recordList;
+            } catch (IOException e) {
+                // Handle exception...
+            }
+        }
+
+        return this.recordList;
     }
 
     public boolean addMaintenanceRecord(CacheRecord record){
