75b7a3393aab3b89b93605399e89fba988b0c8a9
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-10-28 01:02:25 +0100

    Update config.groovy

--- src/main/resources/hudson/plugins/git/BranchSpec/config.groovy
+++ src/main/resources/hudson/plugins/git/BranchSpec/config.groovy
@@ -6,8 +6,4 @@ f.entry(title:_("Branch Specifier (blank for 'any')"), field:"name") {
     f.textbox(default:"*/master")
 }
 
-f.entry {
-    div() {
-        f.repeatableDeleteButton()
-    }
-}
+f.repeatableDeleteButton()
