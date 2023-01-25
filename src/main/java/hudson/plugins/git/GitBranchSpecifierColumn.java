6430226445386ec5c83c058d7c5f0c859f071c47
Basil Crow <me@basilcrow.com>
2023-01-24 21:15:07 -0800

    undo changes to GitBranchSpecifierColumn

--- src/main/java/hudson/plugins/git/GitBranchSpecifierColumn.java
+++ src/main/java/hudson/plugins/git/GitBranchSpecifierColumn.java
@@ -8,6 +8,7 @@ import hudson.views.ListViewColumnDescriptor;
 import java.util.ArrayList;
 import java.util.List;
 import jenkins.triggers.SCMTriggerItem;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -37,7 +38,7 @@ public class GitBranchSpecifierColumn extends ListViewColumn {
     }
 
     public String breakOutString(List<String> branches) {
-        return String.join(", ", branches);
+        return StringUtils.join(branches, ", ");
     }
 
     @Extension
