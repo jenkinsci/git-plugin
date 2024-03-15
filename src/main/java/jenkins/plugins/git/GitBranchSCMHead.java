c6481e2ded21e4b65eafe4686ec68cc20befdf67
AniketNS <aniketshitole565@gmail.com>
2024-03-15 19:16:18 +0530

    removed unnecessary checking from migrate method

--- src/main/java/jenkins/plugins/git/GitBranchSCMHead.java
+++ src/main/java/jenkins/plugins/git/GitBranchSCMHead.java
@@ -68,17 +68,13 @@ public class GitBranchSCMHead extends SCMHead implements GitSCMHeadMixin {
 
         @Override
         public SCMRevision migrate(@NonNull GitSCMSource source, @NonNull AbstractGitSCMSource.SCMRevisionImpl revision) {
-            if (revision.getHead().getClass() == SCMHead.class) {
-                SCMHead revisionHead = revision.getHead();
-                SCMHead branchHead = migrate(source, revisionHead);
-                if (branchHead != null) {
-                    GitBranchSCMHead gitBranchHead = (GitBranchSCMHead) branchHead;
-                    String revisionHash = revision.getHash();
-                    return new GitBranchSCMRevision(gitBranchHead, revisionHash);
-                }
+            SCMHead revisionHead = revision.getHead();
+            if (revisionHead instanceof GitBranchSCMHead) {
+                GitBranchSCMHead gitBranchHead = (GitBranchSCMHead) revisionHead;
+                String revisionHash = revision.getHash();
+                return new GitBranchSCMRevision(gitBranchHead, revisionHash);
             }
             return null;
         }
-
     }
 }
