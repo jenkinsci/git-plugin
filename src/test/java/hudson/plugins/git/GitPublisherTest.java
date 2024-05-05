3c5119eb7614a8c4f19bf0d80658271196d0f656
Mark Waite <mark.earl.waite@gmail.com>
2024-05-05 03:56:11 -0600

    Remove recently introduced trailing white space

--- src/test/java/hudson/plugins/git/GitPublisherTest.java
+++ src/test/java/hudson/plugins/git/GitPublisherTest.java
@@ -79,7 +79,7 @@ import org.junit.rules.TemporaryFolder;
 
 /**
  * Tests for {@link GitPublisher}
- * 
+ *
  * @author Kohsuke Kawaguchi
  */
 public class GitPublisherTest extends AbstractGitProject {
@@ -681,7 +681,6 @@ public class GitPublisherTest extends AbstractGitProject {
         scm.getExtensions().add(new LocalBranch("integration"));
         project.setScm(scm);
 
-
       project.getPublishersList().add(new TestGitPublisher(
           Collections.emptyList(),
           Collections.singletonList(new BranchToPush("origin", "integration")),
@@ -755,7 +754,7 @@ public class GitPublisherTest extends AbstractGitProject {
         FreeStyleProject project = setupSimpleProject("master");
 
         /*
-         * JOB_NAME seemed like the more obvious choice, but when run from a 
+         * JOB_NAME seemed like the more obvious choice, but when run from a
          * multi-configuration job, the value of JOB_NAME includes an equals
          * sign.  That makes log parsing and general use of the variable more
          * difficult.  JENKINS_SERVER_COOKIE is a characteristic env var which
@@ -886,8 +885,8 @@ public class GitPublisherTest extends AbstractGitProject {
             gitClient.config(GitClient.ConfigLevel.LOCAL, "tag.gpgSign", "false");
             return gitClient;
         }
-    }    
-    
+    }
+
     private boolean existsTag(String tag) throws InterruptedException {
         return existsTagInRepo(testGitClient, tag);
     }
@@ -942,7 +941,4 @@ class LongRunningCommit extends Builder {
 
         return true;
     }
-    
-    
-    
 }
