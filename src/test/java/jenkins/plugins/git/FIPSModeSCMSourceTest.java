cde5b99da7f9fe2ea90e559e8d6143ed5601913d
Olivier Lamy <olamy@apache.org>
2024-08-05 09:50:15 +1000

    Use RealJenkinsRule
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/jenkins/plugins/git/FIPSModeSCMSourceTest.java
+++ src/test/java/jenkins/plugins/git/FIPSModeSCMSourceTest.java
@@ -13,8 +13,8 @@ import org.junit.Test;
 import org.jvnet.hudson.test.FlagRule;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.LoggerRule;
+import org.jvnet.hudson.test.RealJenkinsRule;
 
-import java.io.IOException;
 import java.util.logging.Level;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -25,12 +25,8 @@ import static org.junit.Assert.assertThrows;
 
 public class FIPSModeSCMSourceTest {
 
-    @ClassRule
-    public static final FlagRule<String> FIPS_FLAG =
-            FlagRule.systemProperty(FIPS140.class.getName() + ".COMPLIANCE", "true");
-
-    @Rule
-    public JenkinsRule rule = new JenkinsRule();
+    @Rule public RealJenkinsRule rule = new RealJenkinsRule().omitPlugins("eddsa-api", "trilead-api", "git-tag-message")
+            .javaOptions("-Djenkins.security.FIPS140.COMPLIANCE=true");
 
     @Rule
     public LoggerRule logger = new LoggerRule();
@@ -53,25 +49,28 @@ public class FIPSModeSCMSourceTest {
 
     @Test
     @SuppressWarnings("deprecation")
-    public void remotesAreNotFetchedTest() throws IOException, InterruptedException {
-        GitSCMSource source = new GitSCMSource("http://insecure-repo");
-        // Credentials are null, so we should have no FIPS error
-        logger.record(AbstractGitSCMSource.class, Level.SEVERE);
-        logger.capture(10);
-        TaskListener listener = StreamTaskListener.fromStderr();
-        assertThrows("expected exception as repo doesn't exist", GitException.class, () ->source.fetch(listener));
-        assertThat("We should no see the error in the logs", logger.getMessages().size(), is(0));
+    public void remotesAreNotFetchedTest() throws Throwable {
 
-        // Using creds we should be getting an exception
-        Throwable exception = assertThrows("We're not saving creds", IllegalArgumentException.class, () -> source.setCredentialsId("cred-id"));
-        assertThat(exception.getMessage(), containsString("FIPS requires a secure channel"));
-        assertThat("credentials are not saved", source.getCredentialsId(), nullValue());
+        rule.then( r -> {
+            GitSCMSource source = new GitSCMSource("http://insecure-repo");
+            // Credentials are null, so we should have no FIPS error
+            logger.record(AbstractGitSCMSource.class, Level.SEVERE);
+            logger.capture(10);
+            TaskListener listener = StreamTaskListener.fromStderr();
+            assertThrows("expected exception as repo doesn't exist", GitException.class, () -> source.fetch(listener));
+            assertThat("We should no see the error in the logs", logger.getMessages().size(), is(0));
 
-        // Using old constructor (restricted since 3.4.0) to simulate credentials are being set with unsecure connection
-        // This would be equivalent to a user manually adding credentials to config.xml
-        GitSCMSource anotherSource = new GitSCMSource("fake", "http://insecure", "credentialsId", "", "", true);
-        exception = assertThrows("fetch was interrupted so no credential was leaked", IllegalArgumentException.class, () -> anotherSource.fetch(listener));
-        assertThat("We should have a severe log indicating the error", logger.getMessages().size(), is(1));
-        assertThat("Exception indicates problem", exception.getMessage(), containsString("FIPS requires a secure channel"));
+            // Using creds we should be getting an exception
+            Throwable exception = assertThrows("We're not saving creds", IllegalArgumentException.class, () -> source.setCredentialsId("cred-id"));
+            assertThat(exception.getMessage(), containsString("FIPS requires a secure channel"));
+            assertThat("credentials are not saved", source.getCredentialsId(), nullValue());
+
+            // Using old constructor (restricted since 3.4.0) to simulate credentials are being set with unsecure connection
+            // This would be equivalent to a user manually adding credentials to config.xml
+            GitSCMSource anotherSource = new GitSCMSource("fake", "http://insecure", "credentialsId", "", "", true);
+            exception = assertThrows("fetch was interrupted so no credential was leaked", IllegalArgumentException.class, () -> anotherSource.fetch(listener));
+            assertThat("We should have a severe log indicating the error", logger.getMessages().size(), is(1));
+            assertThat("Exception indicates problem", exception.getMessage(), containsString("FIPS requires a secure channel"));
+        });
     }
 }
