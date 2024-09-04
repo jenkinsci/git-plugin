6f597a64607b4217ab9dc1669f6dfb7d1d4331fc
Vincent Latombe <vincent@latombe.net>
2024-09-04 18:38:40 +0200

    Move hunks getting executed on agents to separate classes
    I see java.lang.NoClassDefFoundError: javax/servlet/ServletException happening on agent side when loading these as lambdas. Not sure where the class is being captured from, but moving these to standalone classes definitely fixes the issue I'm seeing.

---
+++ src/main/java/jenkins/plugins/git/UnsetHooks.java
@@ -0,0 +1,28 @@
+package jenkins.plugins.git;
+
+import hudson.remoting.VirtualChannel;
+import java.io.IOException;
+import java.util.logging.Logger;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.jenkinsci.plugins.gitclient.RepositoryCallback;
+
+/**
+ * Unsets git hooks. This can get remotely executed on agents.
+ */
+class UnsetHooks implements RepositoryCallback<Object> {
+    private static final Logger LOGGER = Logger.getLogger(UnsetHooks.class.getName());
+
+    @Override
+    public Object invoke(Repository repo, VirtualChannel channel) throws IOException, InterruptedException {
+        final StoredConfig repoConfig = repo.getConfig();
+        final String val = repoConfig.getString("core", null, "hooksPath");
+        if (val != null && !val.isEmpty() && !DisableHooks.DISABLED_NIX.equals(val) && !DisableHooks.DISABLED_WIN.equals(val)) {
+            LOGGER.warning(() -> String.format("core.hooksPath explicitly set to %s and will be left intact on %s.", val, repo.getDirectory()));
+        } else {
+            repoConfig.unset("core", null, "hooksPath");
+            repoConfig.save();
+        }
+        return null;
+    }
+}
