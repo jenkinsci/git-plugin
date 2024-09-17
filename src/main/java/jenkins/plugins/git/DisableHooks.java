beda22c6fa693aa37b89f7eceb3909d895e553d9
Vincent Latombe <vincent@latombe.net>
2024-09-17 19:15:11 +0200

    Move hunks getting executed on agents to separate classes (#1652)
    * Move hunks getting executed on agents to separate classes
    
    I see java.lang.NoClassDefFoundError: javax/servlet/ServletException happening on agent side when loading these as lambdas. Not sure where the class is being captured from, but moving these to standalone classes definitely fixes the issue I'm seeing.
    
    * Spotbugs

---
+++ src/main/java/jenkins/plugins/git/DisableHooks.java
@@ -0,0 +1,27 @@
+package jenkins.plugins.git;
+
+import hudson.Functions;
+import hudson.remoting.VirtualChannel;
+import java.io.IOException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.jenkinsci.plugins.gitclient.RepositoryCallback;
+
+/**
+ * Disables git hooks. This can get remotely executed on agents.
+ */
+class DisableHooks implements RepositoryCallback<Object> {
+    private static final long serialVersionUID = 1L;
+
+    static final String DISABLED_WIN = "NUL:";
+    static final String DISABLED_NIX = "/dev/null";
+
+    @Override
+    public Object invoke(Repository repo, VirtualChannel channel) throws IOException, InterruptedException {
+        final String VAL = Functions.isWindows() ? DISABLED_WIN : DISABLED_NIX;
+        final StoredConfig repoConfig = repo.getConfig();
+        repoConfig.setString("core", null, "hooksPath", VAL);
+        repoConfig.save();
+        return null;
+    }
+}
