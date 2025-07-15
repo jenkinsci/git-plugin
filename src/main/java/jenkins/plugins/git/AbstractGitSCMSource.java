049d903e23fb522a5bea7020411e165adaeec0aa
Antonio Muñiz <amunizmartin@gmail.com>
2025-07-15 10:09:40 +0200

    Code review by @olamy

--- src/main/java/jenkins/plugins/git/AbstractGitSCMSource.java
+++ src/main/java/jenkins/plugins/git/AbstractGitSCMSource.java
@@ -400,9 +400,7 @@ public abstract class AbstractGitSCMSource extends SCMSource {
             client.setRemoteUrl(remoteName, getRemote());
             listener.getLogger().println((prune ? "Fetching & pruning " : "Fetching ") + remoteName + "...");
             FetchCommand fetch = client.fetch_();
-            if (!context.wantTags()) {
-                fetch.tags(false);
-            }
+            fetch.tags(context.wantTags());
             fetch = fetch.prune(prune);
 
             URIish remoteURI = null;
