1e249b7005dfa00a247b27efd4a7db95cf9f6cce
Nimbusprogrammer <saitejalukka191@gmail.com>
2025-12-09 17:21:12 +0000

    tests: migrate to @WithGitSampleRepo and assert Pipeline warnings

--- src/test/java/hudson/plugins/git/RelativeTargetDirectoryPipelineTest.java
+++ src/test/java/hudson/plugins/git/RelativeTargetDirectoryPipelineTest.java
@@ -1,43 +1,38 @@
 package hudson.plugins.git;
 
+import jenkins.plugins.git.GitSampleRepoRule;
+import jenkins.plugins.git.junit.jupiter.WithGitSampleRepo;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
-import jenkins.plugins.git.GitSampleRepoRule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 @WithJenkins
+@WithGitSampleRepo
 public class RelativeTargetDirectoryPipelineTest {
 
-    public GitSampleRepoRule sampleRepo = new GitSampleRepoRule();
+    private JenkinsRule r;
+    private GitSampleRepoRule sampleRepo;
 
     @BeforeEach
-    public void setupRepo() throws Throwable {
-        sampleRepo.before();
-    }
+    void beforeEach(JenkinsRule rule, GitSampleRepoRule repo) throws Exception {
+        this.r = rule;
+        this.sampleRepo = repo;
 
-    @AfterEach
-    public void tearDownRepo() {
-        sampleRepo.after();
-    }
-
-    @Test
-    public void warningIsLoggedWhenUsedInPipeline(JenkinsRule j) throws Exception {
-        // 1. Init a git repo
         sampleRepo.init();
         sampleRepo.write("Jenkinsfile", "echo 'hello'");
         sampleRepo.git("add", "Jenkinsfile");
         sampleRepo.git("commit", "--all", "-m", "init");
+    }
 
-        // 2. Create Pipeline Job
-        WorkflowJob job = j.createProject(WorkflowJob.class, "test-job");
+    @Test
+    void warningIsLoggedWhenUsedInPipeline() throws Exception {
+        WorkflowJob job = r.jenkins.createProject(WorkflowJob.class, "test-job");
         String repoUrl = sampleRepo.toString();
 
-        // 3. Script that uses the bad feature
         String pipelineScript =
                 "node {\n" +
                         "  checkout([$class: 'GitSCM',\n" +
@@ -48,8 +43,8 @@ public class RelativeTargetDirectoryPipelineTest {
                         "}\n";
         job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
 
-        // 4. Run and Assert
-        WorkflowRun run = j.buildAndAssertSuccess(job);
-        j.assertLogContains("'Check out to a sub-directory' is not intended for use with Pipeline jobs", run);
+        WorkflowRun run = r.buildAndAssertSuccess(job);
+        r.assertLogContains("'Check out to a sub-directory' is not intended for use with Pipeline jobs", run);
+        r.assertLogContains("Please use the 'dir' step instead", run);
     }
 }
