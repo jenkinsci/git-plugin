d2c7e7b2b784449c7e0c2630741ded1ffdced2dc
CJ Steiner <clintonsteiner@gmail.com>
2026-01-23 03:31:31 -0600

    AbstractGitSCMSourceWantTagsTest.java: add tests over getTagTimestamp

--- src/test/java/jenkins/plugins/git/AbstractGitSCMSourceWantTagsTest.java
+++ src/test/java/jenkins/plugins/git/AbstractGitSCMSourceWantTagsTest.java
@@ -5,8 +5,11 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import hudson.EnvVars;
 import hudson.model.TaskListener;
@@ -63,6 +66,8 @@ class AbstractGitSCMSourceWantTagsTest {
         sampleRepo.write("file", "modified");
         sampleRepo.git("commit", "--all", "--message=" + BRANCH_NAME + "-commit-1");
         sampleRepo.git("tag", LIGHTWEIGHT_TAG_NAME);
+        // Sleep to ensure annotated tag has a different timestamp than lightweight tag
+        Thread.sleep(1100);
         sampleRepo.write("file", "modified2");
         sampleRepo.git("commit", "--all", "--message=" + BRANCH_NAME + "-commit-2");
         sampleRepo.git("tag", "-a", ANNOTATED_TAG_NAME, "-m", "annotated-tag-message");
@@ -187,6 +192,125 @@ class AbstractGitSCMSourceWantTagsTest {
             assertThat("Tag " + tag.getName() + " should have valid timestamp",
                     tag.getTimestamp(), greaterThan(year2000));
         }
+    }
+
+    @Test
+    public void lightweightTagHasCommitTimestamp() throws Exception {
+        source.setTraits(Collections.singletonList(new TagDiscoveryTrait()));
+        Set<SCMHead> heads = source.fetch(LISTENER);
+
+        GitTagSCMHead lightweightTag = heads.stream()
+                .filter(h -> h instanceof GitTagSCMHead && h.getName().equals(LIGHTWEIGHT_TAG_NAME))
+                .map(h -> (GitTagSCMHead) h)
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(lightweightTag, "Lightweight tag should be discovered");
+        assertThat("Lightweight tag should have a timestamp", lightweightTag.getTimestamp(), greaterThan(0L));
+    }
+
+    @Test
+    public void annotatedTagHasValidTimestamp() throws Exception {
+        source.setTraits(Collections.singletonList(new TagDiscoveryTrait()));
+        Set<SCMHead> heads = source.fetch(LISTENER);
+
+        GitTagSCMHead annotatedTag = heads.stream()
+                .filter(h -> h instanceof GitTagSCMHead && h.getName().equals(ANNOTATED_TAG_NAME))
+                .map(h -> (GitTagSCMHead) h)
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(annotatedTag, "Annotated tag should be discovered");
+        assertThat("Annotated tag should have a timestamp", annotatedTag.getTimestamp(), greaterThan(0L));
+    }
+
+    @Test
+    public void lightweightAndAnnotatedTagsHaveDifferentCharacteristics() throws Exception {
+        source.setTraits(Collections.singletonList(new TagDiscoveryTrait()));
+        Set<SCMHead> heads = source.fetch(LISTENER);
+
+        Set<GitTagSCMHead> tags = heads.stream()
+                .filter(h -> h instanceof GitTagSCMHead)
+                .map(h -> (GitTagSCMHead) h)
+                .collect(Collectors.toSet());
+
+        assertThat("Should discover both tags", tags.size(), is(2));
+
+        // Both tags should have valid timestamps
+        for (GitTagSCMHead tag : tags) {
+            long timestamp = tag.getTimestamp();
+            assertThat("Tag " + tag.getName() + " timestamp should be positive", timestamp, greaterThan(0L));
+            // Timestamps should be in milliseconds (modern times are > 1.5 billion ms since epoch)
+            assertThat("Tag " + tag.getName() + " timestamp should be in milliseconds",
+                    timestamp, greaterThan(1500000000000L));
+        }
+    }
+
+    @Test
+    public void allDiscoveredTagsHaveValidTimestamps() throws Exception {
+        source.setTraits(Collections.singletonList(new TagDiscoveryTrait()));
+        Set<SCMHead> heads = source.fetch(LISTENER);
+
+        Set<GitTagSCMHead> tags = heads.stream()
+                .filter(h -> h instanceof GitTagSCMHead)
+                .map(h -> (GitTagSCMHead) h)
+                .collect(Collectors.toSet());
+
+        assertThat("Should discover both tags", tags.size(), is(2));
+
+        // All tags should have timestamps that are:
+        // 1. Greater than year 2000 in milliseconds (946684800000)
+        // 2. Less than or equal to current time
+        long year2000Millis = 946684800000L;
+        long currentTimeMillis = System.currentTimeMillis();
+
+        for (GitTagSCMHead tag : tags) {
+            long timestamp = tag.getTimestamp();
+            assertThat("Tag " + tag.getName() + " timestamp should be after year 2000",
+                    timestamp, greaterThan(year2000Millis));
+            assertThat("Tag " + tag.getName() + " timestamp should not be in the future",
+                    timestamp, lessThanOrEqualTo(currentTimeMillis));
+        }
+    }
+
+    @Test
+    public void annotatedTagHasDifferentTimestampFromLightweightTag() throws Exception {
+        source.setTraits(Collections.singletonList(new TagDiscoveryTrait()));
+        Set<SCMHead> heads = source.fetch(LISTENER);
+
+        GitTagSCMHead lightweightTag = heads.stream()
+                .filter(h -> h instanceof GitTagSCMHead && h.getName().equals(LIGHTWEIGHT_TAG_NAME))
+                .map(h -> (GitTagSCMHead) h)
+                .findFirst()
+                .orElse(null);
+
+        GitTagSCMHead annotatedTag = heads.stream()
+                .filter(h -> h instanceof GitTagSCMHead && h.getName().equals(ANNOTATED_TAG_NAME))
+                .map(h -> (GitTagSCMHead) h)
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(lightweightTag, "Lightweight tag should be discovered");
+        assertNotNull(annotatedTag, "Annotated tag should be discovered");
+
+        long lightweightTimestamp = lightweightTag.getTimestamp();
+        long annotatedTimestamp = annotatedTag.getTimestamp();
+
+        // Lightweight tag uses the commit's timestamp (commit-1)
+        // Annotated tag uses the tagger's timestamp (when the tag was created, after a 1.1 second sleep)
+        // They should have different timestamps due to the sleep in beforeAll
+        assertFalse(
+                lightweightTimestamp == annotatedTimestamp,
+                "Annotated tag timestamp (" + annotatedTimestamp + ") should differ from " +
+                "lightweight tag timestamp (" + lightweightTimestamp + ") " +
+                "since annotated tag uses tagger's timestamp while lightweight uses commit timestamp"
+        );
+
+        // Annotated tag should be newer (created after the sleep)
+        assertThat(
+                "Annotated tag timestamp should be greater than lightweight tag timestamp",
+                annotatedTimestamp, greaterThan(lightweightTimestamp)
+        );
     }
 
     static boolean tagsFetched;
