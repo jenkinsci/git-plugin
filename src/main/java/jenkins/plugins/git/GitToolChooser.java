4befe8d972c3de0d2d14c85686c640b6939ab3ea
SHREYAS MADAKE <madakeshreyas@gmail.com>
2026-08-11 23:52:05 +0530

    Exclude git://github.com from cache alternate URLs

--- src/main/java/jenkins/plugins/git/GitToolChooser.java
+++ src/main/java/jenkins/plugins/git/GitToolChooser.java
@@ -269,8 +269,15 @@ public class GitToolChooser {
             addSuffixVariants(remoteURL, alternatives);
         }
 
+        alternatives.removeIf(GitToolChooser::isObsoleteGitHubGitProtocol);
+
         LOGGER.log(Level.FINE, "Cache repo alternative URLs: {0}", alternatives);
         return alternatives;
+    }
+
+    /** GitHub disabled unauthenticated git://; other providers may still use it. */
+    private static boolean isObsoleteGitHubGitProtocol(@NonNull String url) {
+        return url.startsWith("git://github.com/");
     }
 
     /** Cache the estimated repository size for variants of repository URL */
